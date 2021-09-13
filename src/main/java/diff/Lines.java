b86ef1db1f90e86a2fc3f506d5748b9dd8afe7ad
pmbittner <paul.bittner@uni-ulm.de>
2021-09-13 09:21:02 +0200

    bug prevention: Made Lines readonly to avoid confusion on how to manipulate line numbers of DiffNode as it could be done previously via its DiffLineNumbers and LineNumbers but only DiffLineNumbers is correct

--- src/main/java/diff/Lines.java
+++ src/main/java/diff/Lines.java
@@ -5,8 +5,8 @@ package diff;
  * Mainly used to locate code snippets in source code files and textual diffs.
  */
 public class Lines {
-    private int fromInclusive; // including
-    private int toExclusive; // exluding
+    private final int fromInclusive; // including
+    private final int toExclusive; // exluding
 
     private Lines(int fromInclusive, int toExclusive) {
         this.fromInclusive = fromInclusive;
@@ -27,23 +27,6 @@ public class Lines {
 
     public static Lines FromInclToIncl(int fromInclusive, int toInclusive) {
         return new Lines(fromInclusive, toInclusive + 1);
-    }
-
-    public void set(final Lines other) {
-        this.fromInclusive = other.fromInclusive;
-        this.toExclusive = other.toExclusive;
-    }
-
-    public void setFromInclusive(int fromInclusive) {
-        this.fromInclusive = fromInclusive;
-    }
-
-    public void setToExclusive(int toExclusive) {
-        this.toExclusive = toExclusive;
-    }
-
-    public void setToInclusive(int toInclusive) {
-        this.toExclusive = toInclusive + 1;
     }
 
     public int getFromInclusive() {
