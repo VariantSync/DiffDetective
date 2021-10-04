3f6689b910c1a9e95408db3eda5071d11f88e870
pmbittner <paul.bittner@uni-ulm.de>
2021-10-04 10:44:31 +0200

    made renderPatterns03.sh parameterized

---
+++ linegraph/renderPatterns.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 renderLinegraph.py --recursive --pattern --fontsize 7 "/mnt/c/Users/Paul Bittner/Documents/MyDocuments/Paper/Notes/Papers/VariabilityEditPatternMining/Results/Patterns/$1"
--- linegraph/renderPatterns03.sh
+++
@@ -1,1 +0,0 @@
-python3 renderLinegraph.py --recursive --pattern --fontsize 7 "/mnt/c/Users/Paul Bittner/Documents/MyDocuments/Paper/Notes/Papers/VariabilityEditPatternMining/Results/Patterns/03/results"
