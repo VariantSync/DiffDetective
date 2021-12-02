cdac5586af604df93e95a005fc38ce14f0ac6a05
Kevin Jedelhauser <kevin.jedelhauser@uni-ulm.de>
2021-12-02 18:05:14 +0100

    line graph 2nd test draft

--- src/test/java/LineGraphTest.java
+++ src/test/java/LineGraphTest.java
@@ -1,46 +1,109 @@
+import diff.CommitDiff;
+import diff.PatchDiff;
+import diff.difftree.CommitDiffDiffTreeSource;
 import diff.difftree.DiffTree;
 import diff.difftree.LineGraphImport;
+import diff.difftree.serialize.DiffTreeLineGraphExportOptions;
 import diff.difftree.serialize.DiffTreeLineGraphImportOptions;
 import diff.difftree.serialize.GraphFormat;
 import diff.difftree.serialize.nodeformat.CodeDiffNodeLineGraphImporter;
 import diff.difftree.serialize.treeformat.CommitDiffDiffTreeLabelFormat;
-import org.junit.Test;
-
+import diff.difftree.serialize.treeformat.DiffTreeLabelFormat;
+import diff.serialize.LineGraphExport;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import org.junit.Test;
+import org.pmw.tinylog.Logger;
+import util.IO;
+import util.StringUtils;
 
+/**
+ * For testing the import of a lin graph.
+ */
 public class LineGraphTest {
-
-	@Test
-	public void importLineGraphDiffGraph() {
-		importLineGraph(GraphFormat.DIFFGRAPH);
-	}
 	
+	/**
+	 * Test the import of a line graph.
+	 */
 	@Test
 	public void importLineGraphDiffTree() {
-		importLineGraph(GraphFormat.DIFFTREE);
+		String filePath = "linegraph/data/DiffTreeTestFile.lg";
+		importLineGraph(GraphFormat.DIFFTREE, filePath);
 	}
 	
-	private static void importLineGraph(GraphFormat format) {
-		String lineGraph = readLineGraphFile("linegraph/data/difftrees.lg", StandardCharsets.UTF_8);
+	/**
+	 * Import a line graph.
+	 * 
+	 * @param format {@link GraphFormat}
+	 */
+	private static void importLineGraph(final GraphFormat format, String filePath) {
+		String lineGraph = readLineGraphFile(filePath);
+		CommitDiffDiffTreeLabelFormat treeLabel = new CommitDiffDiffTreeLabelFormat();
+		CodeDiffNodeLineGraphImporter nodeLabel = new CodeDiffNodeLineGraphImporter();
 		DiffTreeLineGraphImportOptions options = new DiffTreeLineGraphImportOptions(format,
-				new CommitDiffDiffTreeLabelFormat(),
-				new CodeDiffNodeLineGraphImporter()
+				treeLabel,
+				nodeLabel
 				);
 		List<DiffTree> diffTrees = LineGraphImport.fromLineGraphFormat(lineGraph, options);
+		checkConsistency(diffTrees);
+		compareDiffTrees(diffTrees, filePath, format, treeLabel, nodeLabel);
 	}
 	
-	private static String readLineGraphFile(String path, Charset encoding) {
+	/**
+	 * Read in line graph.
+	 * 
+	 * @param path Relative path to the line graph
+	 * @return The line graph as a string
+	 */
+	private static String readLineGraphFile(final String path) {
 		try {
 			byte[] encoded = Files.readAllBytes(Paths.get(path));
-			return new String(encoded, encoding);
+			return new String(encoded, StandardCharsets.UTF_8);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return "";
 		}
+	}
+	
+	/**
+	 * Check consistency of {@link DiffTree DiffTrees}.
+	 * 
+	 * @param treeList {@link DiffTree} list
+	 */
+	private static void checkConsistency(final List<DiffTree> treeList) {
+		treeList.stream().forEachOrdered(t -> t.assertConsistency());
+	}
+	
+	private static void compareDiffTrees(List<DiffTree> treeList, String inputFile, GraphFormat format, CommitDiffDiffTreeLabelFormat treeLabel, CodeDiffNodeLineGraphImporter nodeLabel) {
+		DiffTreeLineGraphExportOptions options = new DiffTreeLineGraphExportOptions(format, 
+				new CommitDiffDiffTreeLabelFormat(), 
+				new CodeDiffNodeLineGraphImporter()
+				);
+        final StringBuilder lineGraphOutput = new StringBuilder();
+
+        for (var tree : treeList) {
+        	CommitDiffDiffTreeSource source = (CommitDiffDiffTreeSource) tree.getSource();
+//        	CommitDiffDiffTreeSource source = (CommitDiffDiffTreeSource) treeLabel.readTreeHeaderFromLineGraph(line);
+        	
+        	lineGraphOutput
+        		// TODO trying to restore the tree header
+				.append(DiffTreeLabelFormat.setRawTreeLabel(options.treeParser().writeTreeHeaderToLineGraph(source))) // print "t # $LABEL"
+				.append(StringUtils.LINEBREAK)
+				.append(LineGraphExport.toLineGraphFormat(tree, options).getValue())
+				.append(StringUtils.LINEBREAK)
+				.append(StringUtils.LINEBREAK);
+        }
+        
+        try {
+            Logger.info("Writing file " + inputFile + ".out");
+            IO.write(Paths.get(inputFile + ".out"), lineGraphOutput.toString());
+        } catch (IOException exception) {
+            Logger.error(exception);
+        }
+
 	}
 }
