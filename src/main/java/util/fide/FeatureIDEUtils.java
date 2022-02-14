e6a9ab9444b7b3f5ef6f84f90f068d0b57d5f5aa
pmbittner <paul.bittner@uni-ulm.de>
2022-02-14 17:43:51 +0100

    remove deprecated file

--- src/main/java/util/fide/FeatureIDEUtils.java
+++
@@ -1,9 +0,0 @@
-package util.fide;
-
-import de.ovgu.featureide.fm.core.analysis.cnf.generator.configuration.util.Pair;
-
-public class FeatureIDEUtils {
-    public static <A, B> String toString(final Pair<A, B> p) {
-        return "(" + p.getKey() + ", " + p.getValue() + ")";
-    }
-}
