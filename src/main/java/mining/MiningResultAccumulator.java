f5e273e320e9e0ed9196ae245e6b844c00b69e51
pmbittner <paul.bittner@uni-ulm.de>
2022-02-25 17:37:39 +0100

    bugfix in MiningResultAccumulator

--- src/main/java/mining/MiningResultAccumulator.java
+++ src/main/java/mining/MiningResultAccumulator.java
@@ -30,7 +30,8 @@ public class MiningResultAccumulator {
             DiffTreeMiningResult.storeAsCustomInfo(MetadataKeys.TREEFORMAT),
             DiffTreeMiningResult.storeAsCustomInfo(MetadataKeys.NODEFORMAT),
             DiffTreeMiningResult.storeAsCustomInfo(MetadataKeys.EDGEFORMAT),
-            DiffTreeMiningResult.storeAsCustomInfo(MetadataKeys.REPONAME)
+//            DiffTreeMiningResult.storeAsCustomInfo(MetadataKeys.REPONAME),
+            Map.entry(MetadataKeys.REPONAME, (r, v) -> { /* Ignore repository names as they cannot an shoul not be merged. */ })
     );
 
     public static Map<String, DiffTreeMiningResult> getAllTotalResultsIn(final Path folderPath) throws IOException {
