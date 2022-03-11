d8865e9587f371a71e8b8e837d68316b969433b4
pmbittner <paul.bittner@uni-ulm.de>
2022-03-11 20:35:23 +0100

    more commit statistics

--- src/main/java/mining/FindMedianCommitTime.java
+++ src/main/java/mining/FindMedianCommitTime.java
@@ -17,6 +17,8 @@ import java.util.List;
 public class FindMedianCommitTime {
     public static final int NUM_EXPECTED_COMMITS = 1_708_181;
 
+    public static final Path outputFile = Path.of("speedstatistics.txt");
+
     public static void main(final String[] args) throws IOException {
         if (args.length < 1) {
             throw new IllegalArgumentException("Expected path to input directory but got no arguments!");
@@ -35,21 +37,42 @@ public class FindMedianCommitTime {
 
         final ArrayList<CommitProcessTime> alltimes = new ArrayList<>(NUM_EXPECTED_COMMITS);
 
+        double sum = 0;
         for (final Path p : paths) {
-            parse(p, alltimes);
+            sum += parse(p, alltimes);
         }
 
         final CommitProcessTime[] alltimesArray = alltimes.toArray(CommitProcessTime[]::new);
         Arrays.parallelSort(alltimesArray, Comparator.comparingDouble(CommitProcessTime::milliseconds));
-        Logger.info("Median commit process time is: " + alltimesArray[alltimesArray.length / 2]);
+
+        final StringBuilder results = new StringBuilder();
+        results.append("#: " + alltimesArray.length).append(StringUtils.LINEBREAK);
+        results.append("Total   commit process time is: " + ((sum / 1000.0) / 60.0) + "min").append(StringUtils.LINEBREAK);
+        results.append("Fastest commit process time is: " + alltimesArray[0]).append(StringUtils.LINEBREAK);
+        results.append("Slowest commit process time is: " + alltimesArray[alltimesArray.length - 1]).append(StringUtils.LINEBREAK);
+        results.append("Median  commit process time is: " + alltimesArray[alltimesArray.length / 2]).append(StringUtils.LINEBREAK);
+        results.append("Average commit process time is: " + (sum / ((double) alltimesArray.length)) + "ms").append(StringUtils.LINEBREAK);
+
+        final String resultsStr = results.toString();
+        Logger.info("Results:\n" + resultsStr);
+        IO.write(inputPath.resolve(outputFile), resultsStr);
     }
 
-    private static void parse(final Path file, final List<CommitProcessTime> times) throws IOException {
+    private static double parse(final Path file, final List<CommitProcessTime> times) throws IOException {
         final String fileInput = IO.readAsString(file);
         final String[] lines = fileInput.split(StringUtils.LINEBREAK_REGEX);
+        double sumSeconds = 0;
 
         for (final String line : lines) {
-            times.add(CommitProcessTime.fromString(line));
+            if (!line.isBlank()) {
+                final CommitProcessTime lineTime = CommitProcessTime.fromString(line);
+                sumSeconds += lineTime.milliseconds();
+                times.add(lineTime);
+            } else {
+                Logger.warn("Found blank line \"" + line + "\" in " + file);
+            }
         }
+
+        return sumSeconds;
     }
 }
