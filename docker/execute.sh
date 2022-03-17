c221f63e1baf93c593179b97cbfd107234a5e30e
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2022-03-17 10:57:40 +0100

    Added evaluation to Docker container

--- docker/execute.sh
+++ docker/execute.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-if [ "$1" == '' ]
-then
+if [ "$1" == '' ]; then
   echo "Either fully run DiffDetective as presented in the paper (replication), run the proofs (proofs) or a do quick setup validation (validation)."
   echo "-- Examples --"
   echo "Run replication: './experiment.sh replication'"
@@ -10,25 +9,30 @@ then
   exit
 fi
 cd /home/sherlock || exit
-if [ "$1" == 'replication' ]
-then
-    echo "Running full replication."
-    cd holmes || exit
-    java -jar DiffDetectiveRunner.jar
-    cp -r results/* ../results/
-elif [ "$1" == 'validation' ]
-then
+if [ "$1" == 'replication' ] || [ "$1" == 'validation' ]; then
+  cd holmes || exit
+  if [ "$1" == 'replication' ]; then
+    echo "Running full replication. Depending on your system, this will require several hours or even a few days."
+    java -jar DiffDetectiveRunner.jar docs/datasets.md
+  elif [ "$1" == 'validation' ]; then
     echo "Running a short validation."
-    echo "NOT IMPLEMENTED ... EXIT"
-elif [ "$1" == 'proofs' ]
-then
-    echo "Running the proofs"
-    cd proofs || exit
-    stack run
+    java -jar DiffDetectiveRunner.jar docs/validation.md
+  fi
+  echo "Starting evaluation."
+  cp -r results/* ../results/
+  java -cp DiffDetectiveRunner.jar mining.FindMedianCommitTime ../results/difftrees
+  java -cp DiffDetectiveRunner.jar mining.tablegen.MiningResultAccumulator ../results/difftrees ../results/difftrees
+  echo "Finished replication. The results are located in the 'results' directory."
+elif [ "$1" == 'proofs' ]; then
+  echo "Running the proofs"
+  cd proofs || exit
+  stack run
 else
-    echo "Either fully run DiffDetective as presented in the paper (replication), run the proofs (proof) or a do quick setup validation (validation)."
-    echo "-- Examples --"
-    echo "Run simulation: './experiment.sh replication'"
-    echo "Validate the setup: './experiment.sh validation'"
-    exit
+  echo "INVALID ARGUMENT: $1"
+  echo "Either fully run DiffDetective as presented in the paper (replication), run the proofs (proofs) or a do quick setup validation (validation)."
+  echo "-- Examples --"
+  echo "Run replication: './experiment.sh replication'"
+  echo "Run proofs: './experiment.sh proofs'"
+  echo "Validate the setup: './experiment.sh validation'"
+  exit
 fi
---
+++ docs/validation.md
@@ -0,0 +1,3 @@
+Project name | Domain                  | Source code available (**y**es/**n**o)? | Is it a git repository (**y**es/**n**o)? | Repository URL                                               | Clone URL                                                        | Estimated number of commits
+---|-------------------------|-----------------------------------------|-----------------------------------|--------------------------------------------------------------|------------------------------------------------------------------|---
+xterm | terminal emulator       | y                                       | y                                 | https://github.com/Maximus5/xterm                            | https://github.com/Maximus5/xterm.git                            | 112
