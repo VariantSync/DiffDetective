376eaa3f48730805ad3ea85f3492431cae841373
Kevin Jedelhauser <kevin.jedelhauser@uni-ulm.de>
2022-04-01 14:32:48 +0200

    lattice linegraph

--- lattice/task.md
+++ lattice/task.md
@@ -7,3 +7,19 @@ lattice referenziert die patterns und beschreibt die subgraph relation zwischen 
 
 - Was ist T?
 - Sind Lattices vs Subgraphs verbuggt?
+
+## 1.4.22
+1. NODER_PARSER in Z. 247 in renderLineGraph
+   - fehlt hier ein "global NODER_PARSER"?
+   - Variable wird nicht übrschrieben?
+   - <https://thispointer.com/python-how-to-use-global-variables-in-a-function/>
+
+2. Hierarche von Teilgraphen nicht
+   - https://graphviz.org/docs/attrs/rank/
+   - https://graphviz.org/Gallery/undirected/fdpclust.html
+   - http://www.graphviz.org/pdf/dot.1.pdf
+   - https://graphviz.readthedocs.io/en/stable/examples.html
+ 
+   Interessante Seiten:
+   - https://causalnex.readthedocs.io/en/latest/03_tutorial/03_plotting_tutorial.html
+   - https://pygraphviz.github.io/documentation/stable/pygraphviz.pdf
--- linegraph/graphGeneration.py
+++ linegraph/graphGeneration.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python3
+#!/bin/python3
 
-import graphviz
-
-
-d = graphviz.Digraph(filename='createGraph.gv', engine='fdp')
 
 # constants from our Java code
 JAVA_TREE_NAME_SEPARATOR = "$$$"
@@ -25,17 +21,10 @@ CODE_TYPE_CODE_COLOR = '#3399ff'
 CODE_TYPE_OTHER_COLOR = 'black'
 TYPE_BORDER_SIZE = (8.0 / 7.0)
 
-# drawing parameters
-NODE_SIZE = 700
-EDGE_SIZE = 0.5
-ARROW_SIZE = 5
-SHOW_LABELS = True
-DPI = 300
-FONT_SIZE = 3
-
-# other parameters
-WITH_TITLE = True
-INDEX_OUTPUT_FILENAME = False
+# edge colours
+EDGE_A_COLOUR = "#bbeb37"
+EDGE_B_COLOUR = "#ff9129"
+EDGE_AB_COLOUR = "black"
 
 RELEASE_PATTERNS_CODE_PREFIX = "c"
 RELEASE_PATTERNS_MACRO_PREFIX = "m"
@@ -235,140 +224,4 @@ def parseNodeReleaseAtomics(id, name):
     else:
         raise Exception("Node " + name + " has unknown type. Expected prefix " + RELEASE_PATTERNS_CODE_PREFIX + " or " + RELEASE_PATTERNS_MACRO_PREFIX + " but was none.")
 
-    return result
-
-
-def nodeBorderColour(node):
-    return ""
-
-
-def nodeColour(node):
-    return "yellow"
-
-
-def edgeColour(edge):
-    if edge == "a":
-        return "#bbeb37"
-    elif edge == "b":
-        return "#ff9129"
-    else:
-        return "black"
-
-
-def drawNode(cluster, tree, nodeId, nodeLabel):
-    cluster.node(
-        tree + "_" + nodeId, 
-        " (" + nodeId + ")", 
-        color = nodeBorderColour(nodeLabel), 
-        fillcolor = nodeColour(nodeLabel), style = "filled")
-
-
-def drawEdge(cluster, tree, childNodeId, parentNodeId, nodeLabel):
-    cluster.edge(
-        tree + "_" + childNodeId, 
-        tree + "_" + parentNodeId,
-        color=edgeColour(nodeLabel))
-
-
-def drawCluster(tree, nodes, edges):
-    with d.subgraph(name = ("cluster_" + tree)) as c:
-        c.attr(label = "tree " + tree, overlap='false', sep="+10")
-        for v in nodes:
-            drawNode(c, tree, v[0], v[1])
-        for e in edges:
-            drawEdge(c, tree, e[0], e[1], e[2])
-
-
-def patterns():
-    patternsFile = open("patterns.lg", "r")
-    patternLines = patternsFile.readlines()
-
-    trees = {}
-    nodes = []
-    edges = []
-
-    for line in patternLines:
-        line = line.replace("\n", "")
-        line = line.replace("\r", "")
-        
-        if line.startswith("t"):
-            # vorherigen Graph speichern
-            if len(nodes):
-                trees.update({tree : (nodes.copy(), edges.copy())})
-                drawCluster(tree, nodes, edges)
-            
-            # neuen Graph einlesen
-            nodes.clear()
-            edges.clear()
-            # t # TREE_ID
-            lineParams = line.split(" ")
-            tree = lineParams[2]
-            
-        elif line.startswith("v"):
-            # v ID LABEL
-            lineParams = line.split(" ")
-            nodes.append((lineParams[1], lineParams[2]))
-            
-        elif line.startswith("e"):
-            # e NODE_CHILD_ID NODE_PARENT_ID LABEL
-            lineParams = line.split(" ")
-            edges.append((lineParams[1], lineParams[2], lineParams[3]))
-
-    trees.update({tree : (nodes.copy(), edges.copy())})
-    drawCluster(tree, nodes, edges)
-
-
-def lattice():
-    latticeFile = open("lattice.lg", "r")
-    latticeLines = latticeFile.readlines()
-
-    latticeNodes = {}
-    #latticeEdges = []
-
-    for line in latticeLines:
-        line = line.replace("\n", "")
-        line = line.replace("\r", "")
-        
-        if line.startswith("t"):
-            # t # TREE_ID
-            pass
-        
-        elif line.startswith("v"):
-            # v ID LABEL
-            lineParams = line.split(" ")
-            latticeNodes.update({lineParams[1]: lineParams[2]})
-
-            
-        elif line.startswith("e"):
-            # e NODE_CHILD_ID NODE_PARENT_ID LABEL
-            lineParams = line.split(" ")
-            child = lineParams[1]
-            parent = lineParams[2]
-    #        latticeEdges.append((child, parent))
-            d.edge("cluster_" + latticeNodes.get(child), "cluster_" + latticeNodes.get(parent))
-            #d.edge(latticeNodes.get(child) + "_0", latticeNodes.get(parent) + "_" + str(len(trees.get(latticeNodes.get(parent)))), ltail = "cluster_" + latticeNodes.get(child), lhead = "cluster_" + latticeNodes.get(parent))
-
-
-
-def main():
-    #d.attr(rankdir='LR')
-    d.attr(overlap='false')
-    #d.attr(compound='true')
-    
-    d.attr(sep = "+10")
-    
-    patterns()
-    lattice()
-    
-    
-    d.view()
-
-
-#d.node('level0', 'Level')
-#d.node('root', 'T')
-
-
-#with d.subgraph() as s:
-#    s.attr(rank='same')
-
-main()
+    return result 
