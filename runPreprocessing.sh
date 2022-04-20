435df0bc3777d73e34435284ce3bf0c4d7defca5
pmbittner <paul.bittner@uni-ulm.de>
2022-04-20 20:16:26 +0200

    update preprocessing script to new package structure

--- runPreprocessing.sh
+++ runPreprocessing.sh
@@ -1,2 +1,2 @@
-java -cp "mining/lib/DiffDetective-1.0.0-jar-with-dependencies.jar" mining.DiffTreeMiner
+java -cp "target/diffdetective-1.0.0-jar-with-dependencies.jar" org.variantsync.diffdetective.mining.DiffTreeMiner
 echo "runPreprocessing.sh DONE"
