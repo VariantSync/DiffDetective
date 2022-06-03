cf109459f9f7d8a79e0c1bdeab2ef47408b3d9a9
pmbittner <paul.bittner@uni-ulm.de>
2022-06-03 16:25:55 +0200

    parameters for runPostprocessing.sh -ct

--- runPostprocessing.sh
+++ runPostprocessing.sh
@@ -1,2 +1,5 @@
-java -cp "target/diffdetective-1.0.0-jar-with-dependencies.jar" org.variantsync.diffdetective.mining.postprocessing.MiningPostprocessing "results/fsg/difftrees_pilot/all_results" "results/compositeCandidates"
+input_folder=$1
+output_folder=$2
+
+java -cp "target/diffdetective-1.0.0-jar-with-dependencies.jar" org.variantsync.diffdetective.mining.postprocessing.MiningPostprocessing $input_folder $output_folder
 echo "runPostprocessing.sh DONE"
