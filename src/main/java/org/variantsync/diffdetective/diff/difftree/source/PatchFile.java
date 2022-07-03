edfc6d9bd1aa04c39ca62aa44662d8b7fec2c936
pmbittner <paul.bittner@uni-ulm.de>
2022-07-03 13:53:50 +0200

    documentation for diff.difftree.source

--- src/main/java/org/variantsync/diffdetective/diff/difftree/source/PatchFile.java
+++ src/main/java/org/variantsync/diffdetective/diff/difftree/source/PatchFile.java
@@ -4,6 +4,9 @@ import org.variantsync.diffdetective.diff.difftree.DiffTreeSource;
 
 import java.nio.file.Path;
 
+/**
+ * A source for DiffTree's that were created from patch files on disk.
+ * @param path Path to the patch file that was parsed to a DiffTree.
+ */
 public record PatchFile(Path path) implements DiffTreeSource {
-
 }
--- src/main/java/org/variantsync/diffdetective/diff/difftree/source/PatchString.java
+++ src/main/java/org/variantsync/diffdetective/diff/difftree/source/PatchString.java
@@ -3,4 +3,8 @@ package org.variantsync.diffdetective.diff.difftree.source;
 import org.variantsync.diffdetective.diff.TextBasedDiff;
 import org.variantsync.diffdetective.diff.difftree.DiffTreeSource;
 
+/**
+ * Source for DiffTrees that were created from a patch given as a String.
+ * @param getDiff The patch as a String.
+ */
 public record PatchString(String getDiff) implements TextBasedDiff, DiffTreeSource { }
