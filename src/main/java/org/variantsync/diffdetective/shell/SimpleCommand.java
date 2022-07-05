0a87e9b380831c1db4bf5148c3bf41075e6f2049
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2022-07-05 13:09:19 +0200

    Document the `shell` package

--- src/main/java/org/variantsync/diffdetective/shell/SimpleCommand.java
+++ src/main/java/org/variantsync/diffdetective/shell/SimpleCommand.java
@@ -1,8 +1,16 @@
 package org.variantsync.diffdetective.shell;
 
+/** Single executable command with arguments. */
 public class SimpleCommand extends ShellCommand {
     private final String[] parts;
 
+    /**
+     * Constructs a single command.
+     * The first argument has to be a path to an executable which will be given all of the
+     * remaining arguments as parameters on execution.
+     *
+     * @param cmd executable path and arguments for the executable
+     */
     public SimpleCommand(final String... cmd) {
         parts = cmd;
     }
