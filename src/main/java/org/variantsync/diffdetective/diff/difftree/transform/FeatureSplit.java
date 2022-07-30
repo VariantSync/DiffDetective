db36ee06caf2beaa7420b55da1ad400916b8fc90
Lukas Bormann <mail@lukas-bormann.com>
2022-07-31 00:55:43 +0200

    feat: Implemented cluster generation

--- src/main/java/org/variantsync/diffdetective/diff/difftree/transform/FeatureSplit.java
+++ src/main/java/org/variantsync/diffdetective/diff/difftree/transform/FeatureSplit.java
@@ -1,28 +1,73 @@
 package org.variantsync.diffdetective.diff.difftree.transform;
 
+import org.prop4j.Node;
+import org.variantsync.diffdetective.analysis.logic.SAT;
 import org.variantsync.diffdetective.diff.difftree.DiffNode;
 import org.variantsync.diffdetective.diff.difftree.DiffTree;
 import org.variantsync.diffdetective.diff.difftree.Duplication;
+import org.variantsync.diffdetective.feature.PropositionalFormulaParser;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class FeatureSplit {
 
+    public List<DiffTree> featureSplit(DiffTree initDiffTree, List<String> queries){
+        HashSet<DiffTree> subtrees = generateAllSubtrees(initDiffTree);
+        HashMap<String, List<DiffTree>> clusters = generateClusters(subtrees, queries);
+        return generateFeatureAwareDiffTrees(clusters);
+    }
+
     /**
-     * Returns feature-aware DiffTrees based on the changes in the input DiffTree
+     * Returns feature-aware DiffTrees based on the given clusters
      *
-     * @param initDiffTree contains all changes
-     * @param Feature feature which is checked for occurrence
+     * @param clusters contains the grouped subtrees
      * @return a List of subtrees that represent changes to one feature or one composite feature
      */
-    public List<DiffTree> generateFeatureAwareDiffTrees(DiffTree initDiffTree, String Feature){
-        HashSet<DiffTree> subtrees = generateAllSubtrees(initDiffTree);
+    public List<DiffTree> generateFeatureAwareDiffTrees(HashMap<String, List<DiffTree>> clusters){
         //TODO: Add methods to combine subtrees to feature-aware commits
 
         return null;
+    }
+
+    /**
+     * Generates a cluster for each given query and a "remaining" cluster for all non-selected subtrees
+     * @param query: feature mapping which represents a query.
+     * @return true if query is implied in the presence condition
+     */
+    public HashMap<String, List<DiffTree>> generateClusters(HashSet<DiffTree> subtrees, List<String> queries){
+        HashMap<String, List<DiffTree>> clusters = new HashMap<>();
+        clusters.put("remains", new ArrayList<>());
+
+        for (DiffTree subtree: subtrees){
+            boolean hasFound = false;
+            for (String query: queries){
+                if (evaluate(subtree, query)){
+                    if(clusters.containsKey(query)) clusters.put(query, new ArrayList<>());
+                    clusters.get(query).add(subtree); // call by reference
+                    hasFound = true;
+                    break;
+                }
+            }
+            if (!hasFound) clusters.get("remains").add(subtree);
+        }
+        return clusters;
+    }
+
+    /**
+     * compares every node in the subtree with the query
+     * @return true if query is implied in a presence condition of a node
+     */
+    private Boolean evaluate(DiffTree subtree, String query){
+        return subtree.anyMatch(node -> node.isCode() && (satSolver(node.getAfterPresenceCondition(), query) || satSolver(node.getAfterPresenceCondition(), query)));
+    }
+
+    /**
+     * Checks if the query and the presence condition intersect, by checking implication.
+     * @param query: feature mapping which represents a query.
+     * @return true if query is implied in the presence condition
+     */
+    private boolean satSolver(Node presenceCondition, String query) {
+        return SAT.implies(presenceCondition, PropositionalFormulaParser.Default.parse(query));
     }
 
     /**
