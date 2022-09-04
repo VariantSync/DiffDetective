797ab0a7eea1a9a195a24c18ffc6a751c860e4a7
Lukas Bormann <mail@lukas-bormann.com>
2022-09-05 00:11:04 +0200

    feat: added custom metadatakeys and custom HistoryAnalysis

---
+++ src/main/java/org/variantsync/diffdetective/analysis/FeatureSplitMetadataKeys.java
@@ -0,0 +1,11 @@
+package org.variantsync.diffdetective.analysis;
+
+public final class FeatureSplitMetadataKeys {
+    public static final String PATCH_STATS = "patch stats";
+    public static final String NUM_OF_FEATURE_AWARE_PATCHES = "number of feature-aware patches";
+    public static final String PATCH_TIME_MS = "patch time MS";
+    public static final String IS_CONSISTENT = "is consistent";
+    public static final String NUM_OF_FEATURES = "num of features";
+    public static final String FEATURE_AWARE_DIFF_SIZE = "feature-aware diff size";
+
+}
