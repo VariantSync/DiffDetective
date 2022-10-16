215747810453d1c41e98329695e99616a45f101f
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2022-10-16 13:44:19 +0200

    Fix two node format descriptions

--- src/main/java/org/variantsync/diffdetective/diff/difftree/serialize/nodeformat/MappingsDiffNodeFormat.java
+++ src/main/java/org/variantsync/diffdetective/diff/difftree/serialize/nodeformat/MappingsDiffNodeFormat.java
@@ -4,8 +4,8 @@ import org.variantsync.diffdetective.diff.difftree.DiffNode;
 
 /**
  * Print NodeType and DiffType and Mappings of Annotations.
- * The produced label will be <code>NodeType"annotation formula"</code> for mapping nodes,
- * and <code>NodeType""</code> for non-mapping nodes.
+ * The produced label will be <code>DiffType_NodeType_"annotation formula"</code> for mapping nodes,
+ * and <code>DiffType_NodeType_""</code> for non-mapping nodes.
  * @see DiffNodeLabelPrettyfier#prettyPrintIfAnnotationOr(DiffNode, String)
  * @author Paul Bittner, Kevin Jedelhauser
  */
