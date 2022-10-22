e52b2cdea68d865fd116c525b0e27b36aa128223
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2022-10-22 12:13:42 +0200

    Replace tests for parsing conditional macros

--- src/test/java/CPPParserTest.java
+++ src/test/java/CPPParserTest.java
@@ -1,103 +1,109 @@
-import org.junit.Assert;
-import org.junit.Test;
-import org.prop4j.And;
-import org.prop4j.Literal;
-import org.prop4j.Node;
-import org.variantsync.diffdetective.datasets.DatasetDescription;
-import org.variantsync.diffdetective.datasets.DatasetFactory;
-import org.variantsync.diffdetective.diff.PatchReference;
-import org.variantsync.diffdetective.diff.difftree.DiffTree;
 import org.variantsync.diffdetective.diff.difftree.parse.IllFormedAnnotationException;
-import org.variantsync.diffdetective.diff.result.DiffError;
-import org.variantsync.diffdetective.feature.CPPAnnotationParser;
 import org.variantsync.diffdetective.feature.CPPDiffLineFormulaExtractor;
-import org.variantsync.functjonal.Result;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.List;
 
 public class CPPParserTest {
-    private record TestCase(
-            String diffLine,
-            String expectedExtractionResult,
-            Node expectedFormula
-    ) {}
+    private static record TestCase(String formula, String expected) {}
+    private static record ThrowingTestCase(String formula) {}
 
-    private static final Path reposPathWSL = Paths.get("\\\\wsl$", "Ubuntu/home/bittner/VariantSync", "DiffDetectiveMining");
+    private static List<TestCase> testCases() {
+        return List.of(
+            new TestCase("#if A", "A"),
+            new TestCase("#ifdef A", "A"),
+            new TestCase("#ifndef A", "!(A)"),
+            new TestCase("#elif A", "A"),
 
-    private static TestCase tc(
-            String diffLine,
-            String expectedExtractionResult,
-            Node expectedFormula) {
-        return new TestCase(diffLine, expectedExtractionResult, expectedFormula);
-    }
+            new TestCase("#if !A", "!A"),
+            new TestCase("#if A && B", "A&&B"),
+            new TestCase("#if A || B", "A||B"),
+            new TestCase("#if A && (B || C)", "A&&(B||C)"),
 
-    private static final Literal A = new Literal("A");
-    private static final Literal B = new Literal("B");
+            new TestCase("#if 1 > -42", "1__GT____SUB__42"),
+            new TestCase("#if 1 > +42", "1__GT____ADD__42"),
+            new TestCase("#if 42 > A", "42__GT__A"),
+            new TestCase("#if 42 > ~A", "42__GT____NOT__A"),
+            new TestCase("#if A + B > 42", "A__ADD__B__GT__42"),
+            new TestCase("#if A << B", "A__LSHIFT__B"),
+            new TestCase("#if A ? B : C", "A__THEN__B__ELSE__C"),
+            new TestCase("#if A >= B && C > D", "A__GEQ__B&&C__GT__D"),
+            new TestCase("#if A * (B + C)", "A__MUL____LB__B__ADD__C__RB__"),
+            new TestCase("#if defined(A) && (B * 2) > C", "A&&__LB__B__MUL__2__RB____GT__C"),
 
-    private static final List<TestCase> EXTRACTOR_TEST_CASES = List.of(
-            tc(
-                    "#if x + 3 > 1",
-                    "x__ADD__3__GT__1",
-                    new Literal("x__ADD__3__GT__1")
-            ),
-            tc(
-                    "#if A && B",
-                    "A&&B",
-                    new And(A, B)
-            ),
-            tc(
-                    "#if defined(A) && (B * 2) > C",
-                    "A&&__LB__B__MUL__2__RB____GT__C",
-                    new And(A, new Literal("__LB__B__MUL__2__RB____GT__C"))
-            )
-    );
+            new TestCase("#if A // Comment && B", "A"),
+            new TestCase("#if A /* Comment */ && B", "A&&B"),
 
-    @Test
-    public void testFormulaExtractor() throws IllFormedAnnotationException {
-        final CPPDiffLineFormulaExtractor extractor = new CPPDiffLineFormulaExtractor();
-        for (final var testcase : EXTRACTOR_TEST_CASES) {
-            Assert.assertEquals(
-                    testcase.expectedExtractionResult(),
-                    extractor.extractFormula(testcase.diffLine())
-            );
-        }
-    }
+            new TestCase("#if A == B", "A__EQ__B"),
+            new TestCase("#if A == 1", "A__EQ__1"),
 
-    @Test
-    public void testFormulaParser() throws IllFormedAnnotationException {
-        final CPPAnnotationParser parser = CPPAnnotationParser.Default;
-        for (final var testcase : EXTRACTOR_TEST_CASES) {
-            final Node parsedExtractionResult = parser.parseCondition(testcase.expectedExtractionResult());
-            final Node parsedDiffline = parser.parseDiffLine(testcase.diffLine());
-            final Node expectedFormula = testcase.expectedFormula();
-            Assert.assertEquals(
-                    parsedExtractionResult,
-                    parsedDiffline
-            );
-            Assert.assertEquals(
-                    parsedDiffline,
-                    expectedFormula
-            );
-        }
-    }
+            new TestCase("#if defined A", "A"),
+            new TestCase("#if defined(A)", "A"),
+            new TestCase("#if defined (A)", "A"),
+            new TestCase("#if (defined A)", "__LB__A__RB__"),
+            new TestCase("#if MACRO (A)", "MACRO__LB__A__RB__"),
+            new TestCase("#if MACRO (A, B)", "MACRO__LB__A__B__RB__"),
+            new TestCase("#if MACRO (A, B + C)", "MACRO__LB__A__B__ADD__C__RB__"),
+            new TestCase("#if MACRO (A, B) == 1", "MACRO__LB__A__B__RB____EQ__1"),
 
-    @Test
-    public void testLibSSHx() throws IOException {
-        final Result<DiffTree, List<DiffError>> dr = DiffTree.fromPatch(
-                new PatchReference(
-                "tests/pkd/pkd_hello.c",
-                "c8f49becfde6777aa73cea3c8aa58a752d2adce4",
-                "f64814b7be533080a7117cd174c3a81d859f4399"
-                ),
-                new DatasetFactory(reposPathWSL).create(DatasetDescription.summary(
-                        "libssh",
-                        "https://gitlab.com/libssh/libssh-mirror"
-                ))
+            new TestCase("#if ifndef", "ifndef")
         );
-
-        Assert.assertTrue(dr.isFailure());
     }
+
+    private static List<ThrowingTestCase> throwingTestCases() {
+        return List.of(
+            // Invalid macro
+            new ThrowingTestCase(""),
+            new ThrowingTestCase("#"),
+            new ThrowingTestCase("ifdef A"),
+            new ThrowingTestCase("#error A"),
+            new ThrowingTestCase("#iferror A"),
+
+            // Empty formula
+            new ThrowingTestCase("#ifdef"),
+            new ThrowingTestCase("#ifdef // Comment"),
+            new ThrowingTestCase("#ifdef /* Comment */"),
+            new ThrowingTestCase("#if defined()")
+        );
+    }
+
+    private static List<TestCase> wontfixTestCases() {
+        return List.of(
+            new TestCase("#if A == '1'", "A__EQ____TICK__1__TICK__"),
+            new TestCase("#if A && (B - (C || D))", "A&&(B__MINUS__LB__C__LOR__D__RB__)")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    public void testCase(TestCase testCase) throws IllFormedAnnotationException {
+        assertEquals(
+            testCase.expected,
+            new CPPDiffLineFormulaExtractor().extractFormula(testCase.formula())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("throwingTestCases")
+    public void throwingTestCase(ThrowingTestCase testCase) {
+        assertThrows(IllFormedAnnotationException.class, () ->
+            new CPPDiffLineFormulaExtractor().extractFormula(testCase.formula)
+        );
+    }
+
+    @Disabled("WONTFIX")
+    @ParameterizedTest
+    @MethodSource("wontfixTestCases")
+    public void wontfixTestCase(TestCase testCase) throws IllFormedAnnotationException {
+        assertEquals(
+            testCase.expected,
+            new CPPDiffLineFormulaExtractor().extractFormula(testCase.formula())
+        );
+    }
+
 }
