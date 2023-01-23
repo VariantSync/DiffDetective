d54f49d2cdd7f1267eb1bd694def25bb50bc21c6
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2023-01-23 11:33:38 +0100

    Factor duplicate code out of the `validation` package

--- src/main/java/org/variantsync/diffdetective/validation/FACommitValidation.java
+++ src/main/java/org/variantsync/diffdetective/validation/FACommitValidation.java
@@ -1,18 +1,12 @@
 package org.variantsync.diffdetective.validation;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.apache.commons.io.FileUtils;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.tinylog.Logger;
 import org.variantsync.diffdetective.analysis.Analysis;
 import org.variantsync.diffdetective.analysis.AnalysisTask;
@@ -22,34 +16,14 @@ import org.variantsync.diffdetective.analysis.FACommitExtractionValidationTask;
 import org.variantsync.diffdetective.analysis.FeatureSplitFeatureExtractionTask;
 import org.variantsync.diffdetective.analysis.FeatureSplitResult;
 import org.variantsync.diffdetective.analysis.strategies.NullStrategy;
-import org.variantsync.diffdetective.datasets.DatasetDescription;
-import org.variantsync.diffdetective.datasets.DatasetFactory;
-import org.variantsync.diffdetective.datasets.DefaultDatasets;
-import org.variantsync.diffdetective.datasets.ParseOptions;
-import org.variantsync.diffdetective.datasets.Repository;
-import org.variantsync.diffdetective.diff.difftree.filter.DiffTreeFilter;
-import org.variantsync.diffdetective.diff.difftree.filter.ExplainedFilter;
-import org.variantsync.diffdetective.diff.difftree.serialize.DiffTreeLineGraphExportOptions;
-import org.variantsync.diffdetective.diff.difftree.serialize.GraphFormat;
-import org.variantsync.diffdetective.diff.difftree.serialize.edgeformat.EdgeLabelFormat;
-import org.variantsync.diffdetective.diff.difftree.serialize.treeformat.CommitDiffDiffTreeLabelFormat;
-import org.variantsync.diffdetective.diff.difftree.transform.CutNonEditedSubtrees;
-import org.variantsync.diffdetective.mining.formats.DirectedEdgeLabelFormat;
-import org.variantsync.diffdetective.mining.formats.MiningNodeFormat;
-import org.variantsync.diffdetective.mining.formats.ReleaseMiningDiffNodeFormat;
-import org.variantsync.diffdetective.util.Assert;
 
 public class FACommitValidation {
-    public static final boolean UPDATE_REPOS_BEFORE_VALIDATION = false;
-    public static final boolean PRINT_LATEX_TABLE = true;
-    public static final int PRINT_LARGEST_SUBJECTS = 3;
-
     public static final FACommitExtractionAnalysisTaskFactory VALIDATION_TASK_FACTORY =
             (repo, differ, outputPath, commits, randomFeatures) -> new FACommitExtractionValidationTask(new AnalysisTask.Options(
                     repo,
                     differ,
                     outputPath,
-                    ValidationExportOptions(repo),
+                    Validation.ValidationExportOptions(repo),
                     new NullStrategy(),
                     commits
             ),
@@ -60,103 +34,14 @@ public class FACommitValidation {
                     repo,
                     differ,
                     outputPath,
-                    ValidationExportOptions(repo),
+                    Validation.ValidationExportOptions(repo),
                     new NullStrategy(),
                     commits
             ));
 
-    public static MiningNodeFormat NodeFormat() {
-        return new ReleaseMiningDiffNodeFormat();
-    }
-
-    private static EdgeLabelFormat EdgeFormat(final MiningNodeFormat nodeFormat) {
-        final EdgeLabelFormat.Direction direction = EdgeLabelFormat.Direction.ParentToChild;
-        return new DirectedEdgeLabelFormat(nodeFormat, false, direction);
-    }
-
-    public static DiffTreeLineGraphExportOptions ValidationExportOptions(final Repository repository) {
-        final MiningNodeFormat nodeFormat = NodeFormat();
-        return new DiffTreeLineGraphExportOptions(
-                GraphFormat.DIFFTREE
-                // We have to ensure that all DiffTrees have unique IDs, so use name of changed file and commit hash.
-                , new CommitDiffDiffTreeLabelFormat()
-                , nodeFormat
-                , EdgeFormat(nodeFormat)
-                , new ExplainedFilter<>(DiffTreeFilter.notEmpty()) // filters unwanted trees
-                , List.of(new CutNonEditedSubtrees())
-                , DiffTreeLineGraphExportOptions.LogError()
-                .andThen(DiffTreeLineGraphExportOptions.RenderError())
-                .andThen(DiffTreeLineGraphExportOptions.SysExitOnError())
-        );
-    }
-
-    public static void printLaTeXTableFor(final List<DatasetDescription> datasets) {
-        Logger.info("Its dangerous outside. Take this!");
-        System.out.println(DatasetDescription.asLaTeXTable(datasets));
-
-        Logger.info("The {} largest systems are:", PRINT_LARGEST_SUBJECTS);
-        final Comparator<DatasetDescription> larger = (a, b) -> {
-            final int ai = Integer.parseInt(a.commits().replaceAll(",", ""));
-            final int bi = Integer.parseInt(b.commits().replaceAll(",", ""));
-            return -Integer.compare(ai, bi);
-        };
-        final List<DatasetDescription> largestDatasets = datasets.stream()
-                .sorted(larger)
-                .limit(PRINT_LARGEST_SUBJECTS)
-                .collect(Collectors.toList());
-        datasets.stream()
-                .filter(m -> m.name().equalsIgnoreCase("Marlin")
-                        || m.name().equalsIgnoreCase("libssh")
-                        || m.name().equalsIgnoreCase("Busybox")
-                        || m.name().equalsIgnoreCase("Godot"))
-                .forEach(largestDatasets::add);
-        largestDatasets.sort(larger);
-        System.out.println(DatasetDescription.asLaTeXTable(largestDatasets));
-    }
 
     public static void main(String[] args) throws IOException {
-        final ParseOptions.DiffStoragePolicy diffStoragePolicy = ParseOptions.DiffStoragePolicy.DO_NOT_REMEMBER;
-
-        final Path inputDir = Paths.get("..", "DiffDetectiveMining");
-        final Path outputDir = Paths.get("results", "validation", "current");
-
-        final List<Repository> repos;
-        final List<DatasetDescription> datasets = DefaultDatasets.loadDefaultDatasets();
-
-        if (PRINT_LATEX_TABLE) {
-            printLaTeXTableFor(datasets);
-        }
-
-        final DatasetFactory miningDatasetFactory = new DatasetFactory(inputDir);
-        repos = datasets.stream().map(miningDatasetFactory::create).collect(Collectors.toList());
-
-        Logger.info("Performing validation on the following repositories:");
-        for (final Repository repo : repos) {
-            repo.setParseOptions(repo.getParseOptions().withDiffStoragePolicy(diffStoragePolicy));
-            Logger.info("  - {} from {}", repo.getRepositoryName(), repo.getRemoteURI());
-        }
-
-        Logger.info("Preloading repositories:");
-        for (final Repository repo : repos) {
-            repo.getGitRepo().run();
-        }
-
-        if (UPDATE_REPOS_BEFORE_VALIDATION) {
-            Logger.info("Updating repositories:");
-            for (final Repository repo : repos) {
-                try {
-                    Assert.assertTrue(repo.getGitRepo().run().pull().call().isSuccessful());
-                } catch (GitAPIException e) {
-                    Logger.error(e, "Failed to pull repository '{}'", repo.getRepositoryName());
-                }
-            }
-        }
-
-        /* ************************ *\
-        |      END OF ARGUMENTS      |
-        \* ************************ */
-
-        Analysis.forEachRepository(repos, outputDir, (repo, repoOutputDir) -> {
+        Validation.run(args, (repo, repoOutputDir) -> {
             Logger.info(" === Begin Feature Extraction {} ===", repo.getRepositoryName());
             var featureExtrationResult = (FeatureSplitResult)Analysis.forEachCommit(
                 repo,
@@ -189,9 +74,5 @@ public class FACommitValidation {
                 new FeatureSplitResult(repo.getRepositoryName(), randomFeatures)
             );
         });
-        Logger.info("Done");
-
-        final String logFile = "log.txt";
-        FileUtils.copyFile(Path.of(logFile).toFile(), outputDir.resolve(logFile).toFile());
     }
 }
--- src/main/java/org/variantsync/diffdetective/validation/ValidationFeatureSplit.java
+++ src/main/java/org/variantsync/diffdetective/validation/ValidationFeatureSplit.java
@@ -1,146 +1,38 @@
 package org.variantsync.diffdetective.validation;
 
-import org.apache.commons.io.FileUtils;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.tinylog.Logger;
-import org.variantsync.diffdetective.analysis.*;
-import org.variantsync.diffdetective.analysis.strategies.NullStrategy;
-import org.variantsync.diffdetective.datasets.*;
-import org.variantsync.diffdetective.diff.difftree.filter.DiffTreeFilter;
-import org.variantsync.diffdetective.diff.difftree.filter.ExplainedFilter;
-import org.variantsync.diffdetective.diff.difftree.serialize.DiffTreeLineGraphExportOptions;
-import org.variantsync.diffdetective.diff.difftree.serialize.GraphFormat;
-import org.variantsync.diffdetective.diff.difftree.serialize.edgeformat.EdgeLabelFormat;
-import org.variantsync.diffdetective.diff.difftree.serialize.treeformat.CommitDiffDiffTreeLabelFormat;
-import org.variantsync.diffdetective.diff.difftree.transform.CutNonEditedSubtrees;
-import org.variantsync.diffdetective.mining.formats.DirectedEdgeLabelFormat;
-import org.variantsync.diffdetective.mining.formats.MiningNodeFormat;
-import org.variantsync.diffdetective.mining.formats.ReleaseMiningDiffNodeFormat;
-import org.variantsync.diffdetective.util.Assert;
-
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+
+import org.variantsync.diffdetective.analysis.Analysis;
+import org.variantsync.diffdetective.analysis.AnalysisTask;
+import org.variantsync.diffdetective.analysis.AnalysisTaskFactory;
+import org.variantsync.diffdetective.analysis.FeatureSplitResult;
+import org.variantsync.diffdetective.analysis.FeatureSplitValidationTask;
+import org.variantsync.diffdetective.analysis.strategies.NullStrategy;
 
 public class ValidationFeatureSplit {
-    public static final boolean UPDATE_REPOS_BEFORE_VALIDATION = false;
-    public static final boolean PRINT_LATEX_TABLE = true;
-    public static final int PRINT_LARGEST_SUBJECTS = 3;
 
     public static final AnalysisTaskFactory<FeatureSplitResult> VALIDATION_TASK_FACTORY =
             (repo, differ, outputPath, commits) -> new FeatureSplitValidationTask(new AnalysisTask.Options(
                     repo,
                     differ,
                     outputPath,
-                    ValidationExportOptions(repo),
+                    Validation.ValidationExportOptions(repo),
                     new NullStrategy(),
                     commits
             ));
 
-    public static MiningNodeFormat NodeFormat() {
-        return new ReleaseMiningDiffNodeFormat();
-    }
-
-    private static EdgeLabelFormat EdgeFormat(final MiningNodeFormat nodeFormat) {
-        final EdgeLabelFormat.Direction direction = EdgeLabelFormat.Direction.ParentToChild;
-        return new DirectedEdgeLabelFormat(nodeFormat, false, direction);
-    }
-
-    public static DiffTreeLineGraphExportOptions ValidationExportOptions(final Repository repository) {
-        final MiningNodeFormat nodeFormat = NodeFormat();
-        return new DiffTreeLineGraphExportOptions(
-                GraphFormat.DIFFTREE
-                // We have to ensure that all DiffTrees have unique IDs, so use name of changed file and commit hash.
-                , new CommitDiffDiffTreeLabelFormat()
-                , nodeFormat
-                , EdgeFormat(nodeFormat)
-                , new ExplainedFilter<>(DiffTreeFilter.notEmpty()) // filters unwanted trees
-                , List.of(new CutNonEditedSubtrees())
-                , DiffTreeLineGraphExportOptions.LogError()
-                .andThen(DiffTreeLineGraphExportOptions.RenderError())
-                .andThen(DiffTreeLineGraphExportOptions.SysExitOnError())
-        );
-    }
-
-    public static void printLaTeXTableFor(final List<DatasetDescription> datasets) {
-        Logger.info("Its dangerous outside. Take this!");
-        System.out.println(DatasetDescription.asLaTeXTable(datasets));
-
-        Logger.info("The {} largest systems are:", PRINT_LARGEST_SUBJECTS);
-        final Comparator<DatasetDescription> larger = (a, b) -> {
-            final int ai = Integer.parseInt(a.commits().replaceAll(",", ""));
-            final int bi = Integer.parseInt(b.commits().replaceAll(",", ""));
-            return -Integer.compare(ai, bi);
-        };
-        final List<DatasetDescription> largestDatasets = datasets.stream()
-                .sorted(larger)
-                .limit(PRINT_LARGEST_SUBJECTS)
-                .collect(Collectors.toList());
-        datasets.stream()
-                .filter(m -> m.name().equalsIgnoreCase("Marlin")
-                        || m.name().equalsIgnoreCase("libssh")
-                        || m.name().equalsIgnoreCase("Busybox")
-                        || m.name().equalsIgnoreCase("Godot"))
-                .forEach(largestDatasets::add);
-        largestDatasets.sort(larger);
-        System.out.println(DatasetDescription.asLaTeXTable(largestDatasets));
-    }
-
+    /**
+     * Main method to start the validation.
+     * @param args Command-line options.
+     * @throws IOException When copying the log file fails.
+     */
     public static void main(String[] args) throws IOException {
-        final ParseOptions.DiffStoragePolicy diffStoragePolicy = ParseOptions.DiffStoragePolicy.DO_NOT_REMEMBER;
-
-        final Path inputDir = Paths.get("..", "DiffDetectiveMining");
-        final Path outputDir = Paths.get("results", "validation", "current");
-
-        final List<Repository> repos;
-        final List<DatasetDescription> datasets = DefaultDatasets.loadDefaultDatasets();
-
-        if (PRINT_LATEX_TABLE) {
-            printLaTeXTableFor(datasets);
-        }
-
-        final DatasetFactory miningDatasetFactory = new DatasetFactory(inputDir);
-        repos = datasets.stream().map(miningDatasetFactory::create).collect(Collectors.toList());
-
-        Logger.info("Performing validation on the following repositories:");
-        for (final Repository repo : repos) {
-            repo.setParseOptions(repo.getParseOptions().withDiffStoragePolicy(diffStoragePolicy));
-            Logger.info("  - {} from {}", repo.getRepositoryName(), repo.getRemoteURI());
-        }
-
-        Logger.info("Preloading repositories:");
-        for (final Repository repo : repos) {
-            repo.getGitRepo().run();
-        }
-
-        if (UPDATE_REPOS_BEFORE_VALIDATION) {
-            Logger.info("Updating repositories:");
-            for (final Repository repo : repos) {
-                try {
-                    Assert.assertTrue(repo.getGitRepo().run().pull().call().isSuccessful());
-                } catch (GitAPIException e) {
-                    Logger.error(e, "Failed to pull repository '{}'", repo.getRepositoryName());
-                }
-            }
-        }
-
-        /* ************************ *\
-        |      END OF ARGUMENTS      |
-        \* ************************ */
-
-        Analysis.forEachRepository(repos, outputDir, (repo, repoOutputDir) ->
+        Validation.run(args, (repo, repoOutputDir) ->
             Analysis.forEachCommit(
                 repo,
                 repoOutputDir,
                 VALIDATION_TASK_FACTORY,
                 new FeatureSplitResult(repo.getRepositoryName())
         ));
-        Logger.info("Done");
-
-        final String logFile = "log.txt";
-        FileUtils.copyFile(Path.of(logFile).toFile(), outputDir.resolve(logFile).toFile());
     }
 }
