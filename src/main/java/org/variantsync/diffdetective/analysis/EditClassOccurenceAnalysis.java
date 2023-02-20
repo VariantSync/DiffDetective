93690bf3327ba4da2b7a072239d4b5c0977103fa
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2023-02-20 12:09:03 +0100

    Merge `PatchStatistics` and `EditClassCount`

--- src/main/java/org/variantsync/diffdetective/analysis/EditClassOccurenceAnalysis.java
+++ src/main/java/org/variantsync/diffdetective/analysis/EditClassOccurenceAnalysis.java
@@ -3,9 +3,14 @@ package org.variantsync.diffdetective.analysis;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.variantsync.diffdetective.diff.git.PatchDiff;
 import org.variantsync.diffdetective.editclass.EditClass;
+import org.variantsync.diffdetective.editclass.EditClassCatalogue;
 import org.variantsync.diffdetective.editclass.proposed.ProposedEditClasses;
 import org.variantsync.diffdetective.metadata.EditClassCount;
 import org.variantsync.diffdetective.util.CSV;
@@ -13,10 +18,51 @@ import org.variantsync.diffdetective.util.FileUtils;
 import org.variantsync.diffdetective.util.IO;
 
 public class EditClassOccurenceAnalysis implements Analysis.Hooks {
+    /**
+     * Statistics for processing a patch in a commit.
+     * @param patchDiff The diff of the processed patch.
+     * @param editClassCount Count statistics for the edit class matched to the edits in the patch.
+     * @author Paul Bittner, Benjamin Moosherr
+     */
+    private static record Counts(
+            PatchDiff patchDiff,
+            Map<EditClass, Integer> editClassCounts
+    ) implements CSV {
+        /**
+         * Creates empty patch statistics for the given catalogue of edit classes.
+         * @param patch The patch to gather statistics for.
+         * @param catalogue A catalogue of edit classes which should be used for classifying edits.
+         */
+        public Counts(final PatchDiff patch, final EditClassCatalogue catalogue) {
+            this(patch, new LinkedHashMap<>());
+            catalogue.all().forEach(e -> editClassCounts.put(e, 0));
+        }
+
+        /**
+         * Increment the count for the given edit class.
+         * The given edit class is assumed to be part of this counts catalog.
+         * @param editClass The edit class whose count to increase by one.
+         * @see Counts(PatchDiff, EditClassCatalogue)
+         */
+        public void increment(final EditClass editClass) {
+            editClassCounts.computeIfPresent(editClass, (p, i) -> i + 1);
+        }
+
+        @Override
+        public String toCSV(final String delimiter) {
+            var counts = editClassCounts
+                .values()
+                .stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(delimiter));
+            return patchDiff.getCommitHash() + delimiter + patchDiff.getFileName() + delimiter + counts;
+        }
+    }
+
     public static final String PATCH_STATISTICS_EXTENSION = ".patchStatistics.csv";
 
-    private List<PatchStatistics> patchStatistics;
-    private PatchStatistics thisPatchesStatistics;
+    private List<Counts> patchStatistics;
+    private Counts thisPatchesStatistics;
 
     @Override
     public void initializeResults(Analysis analysis) {
@@ -30,7 +76,7 @@ public class EditClassOccurenceAnalysis implements Analysis.Hooks {
 
     @Override
     public boolean beginPatch(Analysis analysis) {
-        thisPatchesStatistics = new PatchStatistics(analysis.getCurrentPatch(), ProposedEditClasses.Instance);
+        thisPatchesStatistics = new Counts(analysis.getCurrentPatch(), ProposedEditClasses.Instance);
         return true;
     }
 
@@ -43,7 +89,7 @@ public class EditClassOccurenceAnalysis implements Analysis.Hooks {
                         editClass,
                         analysis.getCurrentCommitDiff()
                 );
-                thisPatchesStatistics.editClassCount().increment(editClass);
+                thisPatchesStatistics.increment(editClass);
             }
         });
 
@@ -60,7 +106,7 @@ public class EditClassOccurenceAnalysis implements Analysis.Hooks {
         exportPatchStatistics(patchStatistics, FileUtils.addExtension(analysis.getOutputFile(), PATCH_STATISTICS_EXTENSION));
     }
 
-    public static void exportPatchStatistics(final List<PatchStatistics> commitTimes, final Path pathToOutputFile) throws IOException {
+    public static void exportPatchStatistics(final List<Counts> commitTimes, final Path pathToOutputFile) throws IOException {
         IO.write(pathToOutputFile, CSV.toCSV(commitTimes));
     }
 }
