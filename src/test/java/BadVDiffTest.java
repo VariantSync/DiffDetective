eb20a90d7105a581d4f41b12fdbc4e4a1b5a12c5
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2023-02-21 14:14:07 +0100

    feat: unit tests for idempotency of toGood after fromGood

--- src/test/java/BadVDiffTest.java
+++ src/test/java/BadVDiffTest.java
@@ -1,20 +1,43 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.tinylog.Logger;
 import org.variantsync.diffdetective.diff.result.DiffParseException;
+import org.variantsync.diffdetective.util.StringUtils;
 import org.variantsync.diffdetective.variation.diff.DiffTree;
 import org.variantsync.diffdetective.variation.diff.bad.BadVDiff;
+import org.variantsync.diffdetective.variation.diff.graph.GraphView;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
 public class BadVDiffTest {
     private static final Path resDir = Constants.RESOURCE_DIR.resolve("bad");
+
     @ParameterizedTest
-    @ValueSource(strings = { "1.diff" })
-    public void test(String filename) throws IOException, DiffParseException {
-        final Path testfile = resDir.resolve(filename);
-        final DiffTree d = DiffTree.fromFile(testfile, false, false);
-        final BadVDiff b = BadVDiff.fromGood(d);
-        System.out.println(b.prettyPrint());
+    @ValueSource(strings = {
+            "1",
+            "const",
+            "diamond",
+            "deep_insertion"
+    })
+    public void toGood_after_fromGood_idempotency(String filename) throws IOException, DiffParseException {
+        final Path testfile = resDir.resolve(filename + ".diff");
+        Logger.debug("Testing " + testfile);
+
+        final DiffTree initialVDiff = DiffTree.fromFile(testfile, false, false);
+        final GraphView initialVDiffAsGraph = GraphView.fromDiffTree(initialVDiff);
+        Logger.debug("Initial:" + StringUtils.LINEBREAK + initialVDiffAsGraph);
+        initialVDiff.assertConsistency();
+
+        final BadVDiff badDiff = BadVDiff.fromGood(initialVDiff);
+        Logger.debug("Bad:" + StringUtils.LINEBREAK + badDiff.prettyPrint());
+
+        final DiffTree goodDiff = badDiff.toGood();
+        final GraphView goodDiffAsGraph = GraphView.fromDiffTree(goodDiff);
+        Logger.debug("Good:" + StringUtils.LINEBREAK + goodDiffAsGraph);
+        goodDiff.assertConsistency();
+
+        Assertions.assertEquals(initialVDiffAsGraph, goodDiffAsGraph);
     }
 }
