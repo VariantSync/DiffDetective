5a64ceb142008fc84f2403bbfb145a7bc884c55c
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2023-03-07 11:15:57 +0100

    feat: query that looks for features in PCs

---
+++ src/main/java/org/variantsync/diffdetective/variation/tree/view/query/FeatureQuery.java
@@ -0,0 +1,12 @@
+package org.variantsync.diffdetective.variation.tree.view.query;
+
+import org.variantsync.diffdetective.variation.tree.VariationTreeNode;
+
+public record FeatureQuery(String featureName) implements Query {
+    @Override
+    public boolean test(VariationTreeNode variationTreeNode) {
+        return variationTreeNode.getPresenceCondition().getLiterals().stream().anyMatch(
+                literal -> featureName().equals(literal.var)
+        );
+    }
+}
