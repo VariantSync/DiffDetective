a1577fd7d18cc8784ac1cbbf5456e0e3c9b1a798
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2023-03-07 11:15:33 +0100

    fix: view now respects all ancestors

--- src/main/java/org/variantsync/diffdetective/variation/tree/view/View.java
+++ src/main/java/org/variantsync/diffdetective/variation/tree/view/View.java
@@ -1,20 +1,21 @@
 package org.variantsync.diffdetective.variation.tree.view;
 
-import org.variantsync.diffdetective.util.Assert;
 import org.variantsync.diffdetective.variation.tree.VariationTree;
 import org.variantsync.diffdetective.variation.tree.VariationTreeNode;
 import org.variantsync.diffdetective.variation.tree.view.query.Query;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class View {
-    private static void inlineAt(final VariationTreeNode v, final Query q) {
+    private static void filterInline(final VariationTreeNode v, final Set<VariationTreeNode> view) {
         /// We assume that d is interesting for q.
         final List<VariationTreeNode> boringChildren = new ArrayList<>(v.getChildren().size());
         for (final VariationTreeNode child : v.getChildren()) {
-            if (q.isInteresting(child)) {
-                inlineAt(child, q);
+            if (view.contains(child)) {
+                filterInline(child, view);
             } else {
                 boringChildren.add(child);
             }
@@ -24,11 +25,24 @@ public class View {
             v.removeChild(boringChild);
         }
     }
-    public static void inline(final VariationTree t, final Query q) {
-        Assert.assertTrue(
-                q.isInteresting(t.root())
-        );
 
-        inlineAt(t.root(), q);
+    private static void addMeAndMyAncestorsTo(final VariationTreeNode n, Set<VariationTreeNode> nodes) {
+        nodes.add(n);
+        final VariationTreeNode p = n.getParent();
+        if (p != null) {
+            addMeAndMyAncestorsTo(p, nodes);
+        }
+    }
+
+    public static void inline(final VariationTree t, final Query q) {
+        final Set<VariationTreeNode> interestingNodes = new HashSet<>();
+
+        t.forAllPreorder(node -> {
+            if (q.isInteresting(node)) {
+                addMeAndMyAncestorsTo(node, interestingNodes);
+            }
+        });
+
+        filterInline(t.root(), interestingNodes);
     }
 }
