2d5fea2b6b16f127c801806a94a28aaea5031b26
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2023-03-09 10:00:07 +0100

    feat: make Validation configurable (instead having hardcoded parameters)

--- src/main/java/org/variantsync/diffdetective/validation/EditClassValidation.java
+++ src/main/java/org/variantsync/diffdetective/validation/EditClassValidation.java
@@ -1,10 +1,5 @@
 package org.variantsync.diffdetective.validation;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.function.BiFunction;
-
 import org.variantsync.diffdetective.analysis.Analysis;
 import org.variantsync.diffdetective.analysis.FilterAnalysis;
 import org.variantsync.diffdetective.analysis.PreprocessingAnalysis;
@@ -12,8 +7,20 @@ import org.variantsync.diffdetective.analysis.StatisticsAnalysis;
 import org.variantsync.diffdetective.datasets.Repository;
 import org.variantsync.diffdetective.editclass.proposed.ProposedEditClasses;
 import org.variantsync.diffdetective.metadata.EditClassCount;
+import org.variantsync.diffdetective.mining.formats.DirectedEdgeLabelFormat;
+import org.variantsync.diffdetective.mining.formats.MiningNodeFormat;
+import org.variantsync.diffdetective.mining.formats.ReleaseMiningDiffNodeFormat;
 import org.variantsync.diffdetective.variation.diff.filter.DiffTreeFilter;
+import org.variantsync.diffdetective.variation.diff.serialize.GraphFormat;
+import org.variantsync.diffdetective.variation.diff.serialize.LineGraphExportOptions;
+import org.variantsync.diffdetective.variation.diff.serialize.edgeformat.EdgeLabelFormat;
+import org.variantsync.diffdetective.variation.diff.serialize.treeformat.CommitDiffDiffTreeLabelFormat;
 import org.variantsync.diffdetective.variation.diff.transform.CutNonEditedSubtrees;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.BiFunction;
 
 /**
  * This is the validation from our ESEC/FSE'22 paper.
@@ -36,15 +43,44 @@ public class EditClassValidation implements Analysis.Hooks {
     );
 
     /**
+     * Returns the node format that should be used for DiffNode IO.
+     */
+    public static MiningNodeFormat NodeFormat() {
+        return new ReleaseMiningDiffNodeFormat();
+    }
+
+    /**
+     * Returns the edge format that should be used for IO of edges in DiffTrees.
+     */
+    private static EdgeLabelFormat EdgeFormat(final MiningNodeFormat nodeFormat) {
+        final EdgeLabelFormat.Direction direction = EdgeLabelFormat.Direction.ParentToChild;
+        return new DirectedEdgeLabelFormat(nodeFormat, false, direction);
+    }
+
+    /**
+     * Creates new export options for running the validation on the given repository.
+     */
+    public static LineGraphExportOptions ValidationExportOptions(final Repository repository) {
+        final MiningNodeFormat nodeFormat = NodeFormat();
+        return new LineGraphExportOptions(
+                GraphFormat.DIFFTREE
+                // We have to ensure that all DiffTrees have unique IDs, so use name of changed file and commit hash.
+                , new CommitDiffDiffTreeLabelFormat()
+                , nodeFormat
+                , EdgeFormat(nodeFormat)
+                , LineGraphExportOptions.LogError()
+                .andThen(LineGraphExportOptions.RenderError())
+                .andThen(LineGraphExportOptions.SysExitOnError())
+        );
+    }
+
+    /**
      * Main method to start the validation.
      * @param args Command-line options.
      * @throws IOException When copying the log file fails.
      */
     public static void main(String[] args) throws IOException {
-//        setupLogger(Level.INFO);
-//        setupLogger(Level.DEBUG);
-
-        Validation.run(args, (repo, repoOutputDir) ->
+        Validation.run(Validation.Options.DEFAULT(args), (repo, repoOutputDir) ->
             Analysis.forEachCommit(() -> AnalysisFactory.apply(repo, repoOutputDir))
         );
     }
@@ -55,7 +91,7 @@ public class EditClassValidation implements Analysis.Hooks {
     }
 
     @Override
-    public boolean analyzeDiffTree(Analysis analysis) throws Exception {
+    public boolean analyzeDiffTree(Analysis analysis) {
         analysis.getCurrentDiffTree().forAll(node -> {
             if (node.isArtifact()) {
                 analysis.get(EditClassCount.KEY).reportOccurrenceFor(
--- src/main/java/org/variantsync/diffdetective/validation/Validation.java
+++ src/main/java/org/variantsync/diffdetective/validation/Validation.java
@@ -1,5 +1,12 @@
 package org.variantsync.diffdetective.validation;
 
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.tinylog.Logger;
+import org.variantsync.diffdetective.analysis.Analysis;
+import org.variantsync.diffdetective.datasets.*;
+import org.variantsync.diffdetective.util.Assert;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,164 +15,116 @@ import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
-import org.apache.commons.io.FileUtils;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.tinylog.Logger;
-import org.variantsync.diffdetective.analysis.Analysis;
-import org.variantsync.diffdetective.datasets.DatasetDescription;
-import org.variantsync.diffdetective.datasets.DatasetFactory;
-import org.variantsync.diffdetective.datasets.DefaultDatasets;
-import org.variantsync.diffdetective.datasets.ParseOptions;
-import org.variantsync.diffdetective.datasets.Repository;
-import org.variantsync.diffdetective.mining.formats.DirectedEdgeLabelFormat;
-import org.variantsync.diffdetective.mining.formats.MiningNodeFormat;
-import org.variantsync.diffdetective.mining.formats.ReleaseMiningDiffNodeFormat;
-import org.variantsync.diffdetective.util.Assert;
-import org.variantsync.diffdetective.variation.diff.serialize.GraphFormat;
-import org.variantsync.diffdetective.variation.diff.serialize.LineGraphExportOptions;
-import org.variantsync.diffdetective.variation.diff.serialize.edgeformat.EdgeLabelFormat;
-import org.variantsync.diffdetective.variation.diff.serialize.treeformat.CommitDiffDiffTreeLabelFormat;
-
+/**
+ * Main entry point for performing large-scale empirical analyses on a range of git repositories.
+ *
+ * @author Paul Bittner, Benjamin Moosherr
+ */
 public class Validation {
+    /**
+     *
+     * @param repositoriesDirectory The directory to which all git repositories will be cloned to.
+     *                              If this directory already contains the required git repositories,
+     *                              those copies will be used.
+     * @param outputDirectory The directory to which any output data of the analysis should be written to.
+     * @param datasetsFile Path to a markdown file containing a table of git repositories to analyze.
+     * @param diffStoragePolicy Policy that decides whether and how the text-based diffs from git should
+     *                          be stored in any parsed DiffTrees during the analyses. The default is
+     *                          {@link ParseOptions.DiffStoragePolicy#DO_NOT_REMEMBER} as storing all
+     *                          the text-diffs as strings can cause a big memory footprint that can be
+     *                          avoided when the text-diffs are not used anyway.
+     * @param preloadReposBeforeAnalysis Decides whether all repositories should be cloned once before the analysis.
+     *                                   If a repository does not have a local clone in {@link #repositoriesDirectory}
+     *                                   it will be cloned before the analysis if preloadReposBeforeAnalysis is set to
+     *                                   true. Otherwise it will be cloned right before it is about to be analysed.
+     * @param pullRepositoriesBeforeAnalysis Decides whether git pull should be run on each repository before analysis.
+     *                                       Takes effect only if {@link #preloadReposBeforeAnalysis} is true.
+     *                                       Does nothing otherwise.
+     */
+    public record Options(
+            Path repositoriesDirectory,
+            Path outputDirectory,
+            Path datasetsFile,
+            ParseOptions.DiffStoragePolicy diffStoragePolicy,
+            boolean preloadReposBeforeAnalysis,
+            /* Determines whether all analyzed repositories should be updated
+             * (i.e., <code>git pull</code>) before the analysis.
+             * This should be false and is false by default to make results comparable.
+             */
+            boolean pullRepositoriesBeforeAnalysis
+    ) {
+        public static Options DEFAULT(final String[] args) {
+            final Path datasetsFile;
+            if (args.length < 1) {
+                datasetsFile = DefaultDatasets.DEFAULT_DATASETS_FILE;
+            } else if (args.length > 1) {
+                throw new IllegalArgumentException("Error: Expected exactly one argument but got " + args.length + "! Expected a path to a datasets markdown file.");
+            } else {
+                datasetsFile = Path.of(args[0]);
+            }
+
+            return new Options(
+                    Paths.get("..", "DiffDetectiveMining"),
+                    Paths.get("results", "validation", "current"),
+                    datasetsFile,
+                    ParseOptions.DiffStoragePolicy.DO_NOT_REMEMBER,
+                    true,
+                    false
+            );
+        }
+    }
+
     private Validation() {
     }
 
     /**
-     * Hardcoded configuration option that determines of all analyzed repositories should be updated
-     * (i.e., <code>git pull</code>) before the validation.
-     * This should be false and is false by default to make results comparable.
+     * Main method to start the analysis on a set of git repositories.
+     * @param options Options that configure the datasets IO process.
+     * @throws IOException When copying the log file to the output directory fails.
      */
-    public static final boolean UPDATE_REPOS_BEFORE_VALIDATION = false;
-//    /**
-//     * Hardcoded configuration option that determines if
-//     */
-//    public static final boolean PRINT_LATEX_TABLE = true;
-//    public static final int PRINT_LARGEST_SUBJECTS = 3;
-
-    /**
-     * Returns the node format that should be used for DiffNode IO.
-     */
-    public static MiningNodeFormat NodeFormat() {
-        return new ReleaseMiningDiffNodeFormat();
-    }
-
-    /**
-     * Returns the edge format that should be used for IO of edges in DiffTrees.
-     */
-    private static EdgeLabelFormat EdgeFormat(final MiningNodeFormat nodeFormat) {
-        final EdgeLabelFormat.Direction direction = EdgeLabelFormat.Direction.ParentToChild;
-        return new DirectedEdgeLabelFormat(nodeFormat, false, direction);
-    }
-
-    /**
-     * Creates new export options for running the validation on the given repository.
-     */
-    public static LineGraphExportOptions ValidationExportOptions(final Repository repository) {
-        final MiningNodeFormat nodeFormat = NodeFormat();
-        return new LineGraphExportOptions(
-                GraphFormat.DIFFTREE
-                // We have to ensure that all DiffTrees have unique IDs, so use name of changed file and commit hash.
-                , new CommitDiffDiffTreeLabelFormat()
-                , nodeFormat
-                , EdgeFormat(nodeFormat)
-                , LineGraphExportOptions.LogError()
-                .andThen(LineGraphExportOptions.RenderError())
-                .andThen(LineGraphExportOptions.SysExitOnError())
-        );
-    }
-
-//    public static void printLaTeXTableFor(final List<DatasetDescription> datasets) {
-//        Logger.info("Its dangerous outside. Take this!");
-//        System.out.println(DatasetDescription.asLaTeXTable(datasets));
-//
-//        Logger.info("The {} largest systems are:", PRINT_LARGEST_SUBJECTS);
-//        final Comparator<DatasetDescription> larger = (a, b) -> {
-//            final int ai = Integer.parseInt(a.commits().replaceAll(",", ""));
-//            final int bi = Integer.parseInt(b.commits().replaceAll(",", ""));
-//            return -Integer.compare(ai, bi);
-//        };
-//        final List<DatasetDescription> largestDatasets = datasets.stream()
-//                .sorted(larger)
-//                .limit(PRINT_LARGEST_SUBJECTS)
-//                .collect(Collectors.toList());
-//        datasets.stream()
-//                .filter(m -> m.name().equalsIgnoreCase("Marlin")
-//                        || m.name().equalsIgnoreCase("libssh")
-//                        || m.name().equalsIgnoreCase("Busybox")
-//                        || m.name().equalsIgnoreCase("Godot"))
-//                .forEach(largestDatasets::add);
-//        largestDatasets.sort(larger);
-//        System.out.println(DatasetDescription.asLaTeXTable(largestDatasets));
-//    }
-
-    /**
-     * Main method to start the validation.
-     * @param args Command-line options.
-     * @throws IOException When copying the log file fails.
-     */
-    public static void run(String[] args, BiConsumer<Repository, Path> validation) throws IOException {
-        final Path datasetsFile;
-        if (args.length < 1) {
-            datasetsFile = DefaultDatasets.DEFAULT_DATASETS_FILE;
-        } else if (args.length > 1) {
-            Logger.error("Error: Expected exactly one argument but got " + args.length + "! Expected a path to a datasets markdown file.");
-            return;
-        } else {
-            datasetsFile = Path.of(args[0]);
+    public static void run(Options options, BiConsumer<Repository, Path> validation) throws IOException {
+        if (!Files.exists(options.datasetsFile())) {
+            Logger.error("The given datasets file \"" + options.datasetsFile() + "\" does not exist.");
         }
 
-        if (!Files.exists(datasetsFile)) {
-            Logger.error("The given datasets file \"" + datasetsFile + "\" does not exist.");
-        }
-
-        final ParseOptions.DiffStoragePolicy diffStoragePolicy = ParseOptions.DiffStoragePolicy.DO_NOT_REMEMBER;
-
-        final Path inputDir = Paths.get("..", "DiffDetectiveMining");
+        final Path inputDir = options.repositoriesDirectory();
         Logger.info("Reading and cloning git repositories from/to: " + inputDir);
 
-        final Path outputDir = Paths.get("results", "validation", "current");
+        final Path outputDir = options.outputDirectory();
         Logger.info("Writing output to: " + outputDir);
 
-        Logger.info("Loading datasets file: " + datasetsFile);
+        Logger.info("Loading datasets file: " + options.datasetsFile());
         final List<Repository> repos;
-        final List<DatasetDescription> datasets = DefaultDatasets.loadDatasets(datasetsFile);
-
-//        if (PRINT_LATEX_TABLE) {
-//            printLaTeXTableFor(datasets);
-//        }
+        final List<DatasetDescription> datasets = DefaultDatasets.loadDatasets(options.datasetsFile());
 
         final DatasetFactory miningDatasetFactory = new DatasetFactory(inputDir);
         repos = datasets.stream().map(miningDatasetFactory::create).collect(Collectors.toList());
 
         Logger.info("Performing validation on the following repositories:");
         for (final Repository repo : repos) {
-            repo.setParseOptions(repo.getParseOptions().withDiffStoragePolicy(diffStoragePolicy));
+            repo.setParseOptions(repo.getParseOptions().withDiffStoragePolicy(options.diffStoragePolicy()));
             Logger.info("  - {} from {}", repo.getRepositoryName(), repo.getRemoteURI());
         }
 
-        Logger.info("Preloading repositories:");
-        for (final Repository repo : repos) {
-            repo.getGitRepo().run();
-        }
-
-        if (UPDATE_REPOS_BEFORE_VALIDATION) {
-            Logger.info("Updating repositories:");
+        if (options.preloadReposBeforeAnalysis()) {
+            Logger.info("Preloading repositories:");
             for (final Repository repo : repos) {
-                try {
-                    Assert.assertTrue(repo.getGitRepo().run().pull().call().isSuccessful());
-                } catch (GitAPIException e) {
-                    Logger.error(e, "Failed to pull repository '{}'", repo.getRepositoryName());
+                repo.getGitRepo().run();
+            }
+
+            if (options.pullRepositoriesBeforeAnalysis()) {
+                Logger.info("Updating repositories:");
+                for (final Repository repo : repos) {
+                    try {
+                        Assert.assertTrue(repo.getGitRepo().run().pull().call().isSuccessful());
+                    } catch (GitAPIException e) {
+                        Logger.error(e, "Failed to pull repository '{}'", repo.getRepositoryName());
+                    }
                 }
             }
         }
 
-        /* ************************ *\
-        |      END OF ARGUMENTS      |
-        \* ************************ */
-
-        Analysis.forEachRepository(repos, outputDir, (repo, repoOutputDir) ->
-            validation.accept(repo, repoOutputDir)
-        );
+        Analysis.forEachRepository(repos, outputDir, validation);
         Logger.info("Done");
 
         final String logFile = "log.txt";
