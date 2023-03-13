c4c9726e211d98f69246e6392916af0a912c7a43
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2023-03-13 11:49:48 +0100

    refactor: use getUniqueContainedFeatures method to get feature names instead of doing that manually

--- src/main/java/org/variantsync/diffdetective/variation/tree/view/query/FeatureQuery.java
+++ src/main/java/org/variantsync/diffdetective/variation/tree/view/query/FeatureQuery.java
@@ -5,8 +5,8 @@ import org.variantsync.diffdetective.variation.tree.VariationTreeNode;
 public record FeatureQuery(String featureName) implements Query {
     @Override
     public boolean test(VariationTreeNode variationTreeNode) {
-        return variationTreeNode.getPresenceCondition().getLiterals().stream().anyMatch(
-                literal -> featureName().equals(literal.var)
+        return variationTreeNode.getPresenceCondition().getUniqueContainedFeatures().stream().anyMatch(
+                otherFeatureName -> featureName().equals(otherFeatureName)
         );
     }
 
