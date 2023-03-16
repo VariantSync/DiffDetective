d7423c0f8a02d1048d89c6d5398b9473bef2fca2
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-03-16 19:43:01 +0100

    deps: added updated functional lib

---
+++ local-maven-repo/deploy-functjonal.sh
@@ -0,0 +1,2 @@
+mvn deploy:deploy-file -DgroupId=org.variantsync -DartifactId=functjonal -Dversion=1.0-SNAPSHOT -Durl=file:../local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=../src/main/resources/lib/functjonal-1.0-SNAPSHOT.jar
+rm -rf ~/.m2/repository/org/variantsync/functjonal/
