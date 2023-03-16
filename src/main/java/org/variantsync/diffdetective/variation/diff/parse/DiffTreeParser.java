6ab613bc022610666196b773f24e4a9c887ab284
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2023-03-17 00:06:55 +0100

    Add `#endif` to the label of the last node it belongs to
    This is necessary to identify which lines contain `#endif`s.
    
    Use case: Incrementally calculate the presence condition of `#endif`
    lines.

--- src/main/java/org/variantsync/diffdetective/variation/diff/parse/DiffTreeParser.java
+++ src/main/java/org/variantsync/diffdetective/variation/diff/parse/DiffTreeParser.java
@@ -21,6 +21,7 @@ import org.variantsync.diffdetective.variation.diff.DiffTree;
 import org.variantsync.diffdetective.variation.diff.DiffType;
 import org.variantsync.diffdetective.variation.NodeType;
 import org.variantsync.diffdetective.variation.diff.Time;
+import org.variantsync.functjonal.list.FilteredMappedListView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -322,6 +323,24 @@ public class DiffTreeParser {
 
         if ("endif".equals(conditionalMacroName)) {
             lastArtifact = null;
+
+            // Add the #endif to last seen #if/#elif/#else it belongs to. Don't add it twice if the
+            // node is #endif and the #if/#elif/#else node are unchanged.
+            // Note: If the #endif has been modified but the #if/#elif/#else node was unchanged
+            //       there will be two #endif lines in the label (they can be distinguished by
+            //       checking at which time the line number is invalid).
+            if (diffType == DiffType.NON && beforeStack.peek() == afterStack.peek()) {
+                beforeStack.peek().addDiffLines(line.getLines());
+            } else {
+                diffType.forAllTimesOfExistence(beforeStack, afterStack, stack ->
+                    stack.peek().addDiffLines(FilteredMappedListView.map(line.getLines(), diffLine ->
+                        new DiffNode.Label.Line(
+                            diffLine.content(),
+                            diffLine.lineNumber().as(stack == beforeStack ? DiffType.REM : DiffType.ADD)
+                        )
+                    ))
+                );
+            }
 
             // Do not create a node for ENDIF, but update the line numbers of the closed if-chain
             // and remove that if-chain from the relevant stacks.
