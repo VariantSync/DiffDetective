c9077008eca55085289f143e622bd13a2f533ff9
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-03-17 12:18:00 +0100

    misc: cleaned code

--- src/test/java/GitDifferTest.java
+++ src/test/java/GitDifferTest.java
@@ -2,39 +2,28 @@ import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.tinylog.Logger;
 import org.variantsync.diffdetective.datasets.PatchDiffParseOptions;
 import org.variantsync.diffdetective.datasets.Repository;
-import org.variantsync.diffdetective.datasets.predefined.Marlin;
 import org.variantsync.diffdetective.diff.git.CommitDiff;
 import org.variantsync.diffdetective.diff.git.DiffFilter;
 import org.variantsync.diffdetective.diff.git.PatchDiff;
 import org.variantsync.diffdetective.diff.result.DiffParseException;
-import org.variantsync.diffdetective.editclass.proposed.ProposedEditClasses;
-import org.variantsync.diffdetective.mining.DiffTreeMiner;
-import org.variantsync.diffdetective.util.Assert;
 import org.variantsync.diffdetective.util.IO;
 import org.variantsync.diffdetective.variation.diff.DiffTree;
 import org.variantsync.diffdetective.variation.diff.parse.DiffTreeParseOptions;
 import org.variantsync.diffdetective.variation.diff.parse.DiffTreeParser;
 import org.variantsync.diffdetective.variation.diff.serialize.Format;
 import org.variantsync.diffdetective.variation.diff.serialize.LineGraphExporter;
-import org.variantsync.diffdetective.variation.diff.serialize.TikzExporter;
 import org.variantsync.diffdetective.variation.diff.serialize.edgeformat.ChildOrderEdgeFormat;
-import org.variantsync.diffdetective.variation.diff.serialize.edgeformat.DefaultEdgeLabelFormat;
 import org.variantsync.diffdetective.variation.diff.serialize.nodeformat.FullNodeFormat;
-import org.variantsync.diffdetective.variation.diff.transform.DiffTreeTransformer;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.variantsync.diffdetective.variation.diff.Time.AFTER;
-import static org.variantsync.diffdetective.variation.diff.Time.BEFORE;
 
 public class GitDifferTest {
     private final static Path testDir = Constants.RESOURCE_DIR.resolve("diffs").resolve("differ");
@@ -65,7 +54,7 @@ public class GitDifferTest {
                 )));
     }
 
-    public void testCase(String commitHash) throws IOException, DiffParseException {
+    public void testCase(String commitHash) throws IOException {
         Repository repo = repo();
         final CommitDiff commitDiff = DiffTreeParser.parseCommit(repo, commitHash);
 
