aadd3b1d56135c98ee7374542eb89e32e64bf57d
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2023-04-04 18:12:43 +0200

    test: new manual test case for all-views algo

--- src/test/java/ViewTest.java
+++ src/test/java/ViewTest.java
@@ -2,15 +2,21 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.prop4j.Implies;
 import org.prop4j.Literal;
+import org.prop4j.Node;
+import org.prop4j.NodeWriter;
 import org.tinylog.Logger;
 import org.variantsync.diffdetective.diff.result.DiffParseException;
+import org.variantsync.diffdetective.experiments.views.ViewAnalysis;
 import org.variantsync.diffdetective.show.Show;
 import org.variantsync.diffdetective.show.engine.GameEngine;
+import org.variantsync.diffdetective.util.StringUtils;
 import org.variantsync.diffdetective.variation.diff.DiffTree;
 import org.variantsync.diffdetective.variation.diff.Time;
 import org.variantsync.diffdetective.variation.diff.bad.BadVDiff;
+import org.variantsync.diffdetective.variation.diff.parse.DiffTreeParseOptions;
+import org.variantsync.diffdetective.variation.diff.view.DiffView;
+import org.variantsync.diffdetective.variation.diff.view.ViewSource;
 import org.variantsync.diffdetective.variation.tree.VariationTree;
-import org.variantsync.diffdetective.variation.tree.source.VariationTreeSource;
 import org.variantsync.diffdetective.variation.tree.view.TreeView;
 import org.variantsync.diffdetective.variation.tree.view.query.FeatureQuery;
 import org.variantsync.diffdetective.variation.tree.view.query.Query;
@@ -19,6 +25,9 @@ import org.variantsync.diffdetective.variation.tree.view.query.VariantQuery;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.variantsync.diffdetective.util.fide.FormulaUtils.*;
 
@@ -116,5 +125,65 @@ public class ViewTest {
                 Show.tree(TreeView.tree(b, configureExample1), "view_{tree}(project_b(D), " + configureExample1.getName() + ")"),
                 Show.tree(TreeView.tree(b, traceYesExample1), "view_{tree}(project_b(D), " + traceYesExample1.getName() + ")")
         );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "1",
+            "2"
+//            "runningexample",
+////            "const",
+////            "diamond",
+////            "deep_insertion"
+    })
+    void testAllConfigs(String filename) throws IOException, DiffParseException {
+        final Path testfile = resDir.resolve(filename + ".diff");
+        final DiffTree d = DiffTree.fromFile(testfile, DiffTreeParseOptions.Default);
+
+        final List<Node> configs = ViewAnalysis.getUniquePartialConfigs(d, false);
+        final List<DiffTree> views = new ArrayList<>();
+
+        final StringBuilder str = new StringBuilder();
+        for (int i = 0; i < configs.size(); ++i) {
+            final Node config = configs.get(i);
+
+            final Query q = new VariantQuery(config);
+            final DiffTree view = DiffView.optimized(d, q);
+            views.add(view);
+
+            str
+                    .append(" ")
+                    .append(org.apache.commons.lang3.StringUtils.leftPad(
+                            Integer.toString(i),
+                            4)
+                    )
+                    .append(".) ")
+                    .append(org.apache.commons.lang3.StringUtils.rightPad(
+                            config.toString(NodeWriter.logicalSymbols),
+                            40)
+                    )
+                    .append(" --- ")
+                    .append(
+                            view.computeArtifactNodes().stream()
+                                    .map(a -> a.getLabel().lines().stream()
+                                            .map(String::trim)
+                                            .collect(Collectors.joining(StringUtils.LINEBREAK))
+                                    )
+                                    .collect(Collectors.toList()))
+                    .append(StringUtils.LINEBREAK);
+        }
+
+        Logger.info("All unique partial configs:" + StringUtils.LINEBREAK + str);
+        Show.diff(d, "D").show();
+//        {
+//            final Query q = new FeatureQuery("C");
+//            Show.diff(DiffView.optimized(d, q), "view(D, " + q.getName() + ")").showAndAwait();
+//        }
+
+        for (int i = 0; i < configs.size(); ++i) {
+            final DiffTree view = views.get(i);
+            final Query       q = ((ViewSource) view.getSource()).q();
+            Show.diff(view, i + ".) view(D, " + q.getName() + ")").showAndAwait();
+        }
     }
 }
