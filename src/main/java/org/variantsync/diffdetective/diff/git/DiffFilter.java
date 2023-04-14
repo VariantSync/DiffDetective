77efc34138995485badb03ab3f23a48a783ade85
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-04-14 11:10:34 +0200

    fix: diffs are now analyzed if they match a filter criterion either before or after a change

--- src/main/java/org/variantsync/diffdetective/diff/git/DiffFilter.java
+++ src/main/java/org/variantsync/diffdetective/diff/git/DiffFilter.java
@@ -279,7 +279,7 @@ public class DiffFilter {
      */
     public boolean filter(DiffEntry diffEntry) {
         if (!allowedPaths.isEmpty() &&
-                !(isAllowedPath(diffEntry.getOldPath()) && isAllowedPath(diffEntry.getNewPath()))) {
+                !(isAllowedPath(diffEntry.getOldPath()) || isAllowedPath(diffEntry.getNewPath()))) {
             return false;
         }
         if (!blockedPaths.isEmpty() &&
@@ -291,7 +291,7 @@ public class DiffFilter {
             return false;
         }
         if (!allowedFileExtensions.isEmpty() &&
-                !(hasAllowedExtension(diffEntry.getOldPath()) && hasAllowedExtension(diffEntry.getNewPath()))) {
+                !(hasAllowedExtension(diffEntry.getOldPath()) || hasAllowedExtension(diffEntry.getNewPath()))) {
             return false;
         }
         if (!blockedFileExtensions.isEmpty() &&
