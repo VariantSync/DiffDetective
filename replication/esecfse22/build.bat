92e91b5e59785218f090ddc510619ab028ed4029
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-06-07 17:24:25 +0200

    docker: extend batch scripts with additional guards

--- replication/esecfse22/build.bat
+++ replication/esecfse22/build.bat
@@ -1,2 +1,18 @@
-docker build -t diff-detective .
-@pause
+@echo off
+setlocal
+
+set "targetSubPath=esecfse22"
+
+rem Get the current directory
+for %%A in ("%CD%") do set "currentDir=%%~nxA"
+
+rem Check if the current directory ends with the target sub-path
+if "%currentDir:~-9%"=="%targetSubPath%" (
+    cd ..\..
+    docker build -t diff-detective .
+    @pause
+) else (
+    echo error: the script must be run from inside the esecfse22 directory, i.e., DiffDetective\replication\%targetSubPath%
+)
+endlocal
+
