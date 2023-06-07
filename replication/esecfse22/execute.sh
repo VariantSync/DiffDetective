10771be97ef96526579b37d9c08b0949556502a8
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-06-07 17:16:46 +0200

    docker: add directory guard to execute script

--- replication/esecfse22/execute.sh
+++ replication/esecfse22/execute.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
+# Assure that the script is only called from the esecfse22 folder
+current_dir=$(pwd)
+expected_path="/replication/esecfse22"
+if [[ ! $current_dir =~ $expected_path ]]; then
+  echo "error: the script must be run from inside the esecfse22 directory, i.e., DiffDetective$expected_path"
+  exit 1
+fi
+
 if [[ $# -gt 0 ]]; then
 echo "Executing $1"
 fi
