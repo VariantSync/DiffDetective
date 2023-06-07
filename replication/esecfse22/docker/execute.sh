773f9778b0e480db8257a1a75a4273df23ef9f79
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-06-07 16:42:55 +0200

    fix(docker): update paths to Java classes

--- replication/esecfse22/docker/execute.sh
+++ replication/esecfse22/docker/execute.sh
@@ -17,19 +17,19 @@ cd /home/sherlock || exit
 
 if [ "$1" == 'replication' ]; then
   echo "Running full replication. Depending on your system, this will require several hours or even a few days."
-  java -cp DiffDetective.jar org.variantsync.diffdetective.validation.EditClassValidation docs/datasets/esecfse22-replication.md
+  java -cp DiffDetective.jar org.variantsync.diffdetective.experiments.esecfse22.EditClassValidation docs/datasets/esecfse22-replication.md
 elif [ "$1" == 'verification' ]; then
   echo "Running a short verification."
-  java -cp DiffDetective.jar org.variantsync.diffdetective.validation.EditClassValidation docs/datasets/esecfse22-verification.md
+  java -cp DiffDetective.jar org.variantsync.diffdetective.experiments.esecfse22.EditClassValidation docs/datasets/esecfse22-verification.md
 else
   echo ""
   echo "Running detection on a custom dataset with the input file $1"
   echo ""
-  java -cp DiffDetective.jar org.variantsync.diffdetective.validation.EditClassValidation "$1"
+  java -cp DiffDetective.jar org.variantsync.diffdetective.experiments.esecfse22.EditClassValidation "$1"
 fi
 echo "Collecting results."
 cp -r results/* ../results/
-java -cp DiffDetective.jar org.variantsync.diffdetective.validation.FindMedianCommitTime ../results/validation/current
+java -cp DiffDetective.jar org.variantsync.diffdetective.experiments.esecfse22.FindMedianCommitTime ../results/validation/current
 java -cp DiffDetective.jar org.variantsync.diffdetective.tablegen.MiningResultAccumulator ../results/validation/current ../results/validation/current
 echo "The results are located in the 'results' directory."
 
