652efa06ecdf3c9650fda0429c480dbbe59b256a
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-06-08 11:14:58 +0200

    fix(docker): correct path to Dockerfile

--- replication/esecfse22/build.bat
+++ replication/esecfse22/build.bat
@@ -9,7 +9,7 @@ for %%A in ("%CD%") do set "currentDir=%%~nxA"
 rem Check if the current directory ends with the target sub-path
 if "%currentDir:~-9%"=="%targetSubPath%" (
     cd ..\..
-    docker build -t diff-detective .
+    docker build -t diff-detective -f replication\esecfse22\Dockerfile .
     @pause
 ) else (
     echo error: the script must be run from inside the esecfse22 directory, i.e., DiffDetective\replication\%targetSubPath%
