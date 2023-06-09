266baf38fdb1036ceb3c11b014c410d49c3f46e2
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2023-06-09 13:38:15 +0200

    Move the results of the replication to their subdirectories
    This is the intended behaviour and already correctly implemented in the
    Windows version.

--- replication/splc23-views/execute.sh
+++ replication/splc23-views/execute.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Assure that the script is only called from the splc23-views folder
-cd "$(dirname "${BASH_SOURCE[0]}")/../.." || exit
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 if [[ $# -gt 0 ]]; then
 echo "Executing $1"
