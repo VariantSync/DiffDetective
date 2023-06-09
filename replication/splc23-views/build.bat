9e25946087ae46804620bd553556318315997ad0
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2023-06-09 11:02:45 +0200

    fix: substring in replication/splc23-views/build.bat

--- replication/splc23-views/build.bat
+++ replication/splc23-views/build.bat
@@ -7,7 +7,7 @@ rem Get the current directory
 for %%A in ("%CD%") do set "currentDir=%%~nxA"
 
 rem Check if the current directory ends with the target sub-path
-if "%currentDir:~-9%"=="%targetSubPath%" (
+if "%currentDir:~-12%"=="%targetSubPath%" (
     cd ..\..
     docker build -t diff-detective-views replication\splc23-views\Dockerfile .
     @pause
