6a45bc008a6840ebcbb738955e98b4805bb30073
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2024-01-27 12:00:31 +0100

    bump version number to 2.1.0

--- scripts/genUltimateResults.sh
+++ scripts/genUltimateResults.sh
@@ -1,4 +1,4 @@
 resultsdir=$1
 
-java -cp "target/diffdetective-2.0.0-jar-with-dependencies.jar" org.variantsync.diffdetective.tablegen.MiningResultAccumulator $resultsdir $resultsdir
+java -cp "target/diffdetective-2.1.0-jar-with-dependencies.jar" org.variantsync.diffdetective.tablegen.MiningResultAccumulator $resultsdir $resultsdir
 echo "genUltimateResults.sh DONE"
