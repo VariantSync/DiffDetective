c4e6e28c8e40733f1f9e91b35009dc843add79a8
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2024-01-27 18:51:34 +0100

    improve toString of VariationDiffSources

--- src/main/java/org/variantsync/diffdetective/variation/diff/source/PatchString.java
+++ src/main/java/org/variantsync/diffdetective/variation/diff/source/PatchString.java
@@ -6,4 +6,9 @@ import org.variantsync.diffdetective.diff.text.TextBasedDiff;
  * Source for VariationDiffs that were created from a patch given as a String.
  * @param getDiff The patch as a String.
  */
-public record PatchString(String getDiff) implements TextBasedDiff, VariationDiffSource { }
+public record PatchString(String getDiff) implements TextBasedDiff, VariationDiffSource {
+    @Override
+    public String toString() {
+        return "from line-based diff " + getDiff;
+    }
+}
