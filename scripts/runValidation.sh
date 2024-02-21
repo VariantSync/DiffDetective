f91123b3d1a98364c930a758b923b1288dd1296a
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2024-02-21 17:10:31 +0100

    Update the version number in the remaining files
    These were missed in 569f6631bd486af445d1df891c901f6006f84778.

--- scripts/runValidation.sh
+++ scripts/runValidation.sh
@@ -1,2 +1,2 @@
-java -cp "target/diffdetective-2.1.0-jar-with-dependencies.jar" org.variantsync.diffdetective.validation.EditClassValidation
+java -cp "target/diffdetective-2.2.0-jar-with-dependencies.jar" org.variantsync.diffdetective.validation.EditClassValidation
 echo "runValidation.sh DONE"
