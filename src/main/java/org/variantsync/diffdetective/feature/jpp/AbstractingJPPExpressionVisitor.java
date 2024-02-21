cbbb3b4f8245e7e47a3fc4aa7f88c034a7cd2585
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-02-21 09:18:29 +0100

    docs: add comments describing the parser rules

--- src/main/java/org/variantsync/diffdetective/feature/jpp/AbstractingJPPExpressionVisitor.java
+++ src/main/java/org/variantsync/diffdetective/feature/jpp/AbstractingJPPExpressionVisitor.java
@@ -3,115 +3,93 @@ package org.variantsync.diffdetective.feature.jpp;
 import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
 import org.variantsync.diffdetective.feature.antlr.CExpressionParser;
 import org.variantsync.diffdetective.feature.antlr.CExpressionVisitor;
+import org.variantsync.diffdetective.feature.antlr.JPPExpressionParser;
+import org.variantsync.diffdetective.feature.antlr.JPPExpressionVisitor;
 
-public class AbstractingJPPExpressionVisitor extends AbstractParseTreeVisitor<StringBuilder> implements CExpressionVisitor<StringBuilder> {
+public class AbstractingJPPExpressionVisitor extends AbstractParseTreeVisitor<StringBuilder> implements JPPExpressionVisitor<StringBuilder> {
+    // expression
+    //    :   logicalOrExpression
+    //    ;
     @Override
-    public StringBuilder visitExpression(CExpressionParser.ExpressionContext ctx) {
+    public StringBuilder visitExpression(JPPExpressionParser.ExpressionContext ctx) {
         return null;
     }
 
+    // logicalOrExpression
+    //    :   logicalAndExpression (OR logicalAndExpression)*
+    //    ;
     @Override
-    public StringBuilder visitConditionalExpression(CExpressionParser.ConditionalExpressionContext ctx) {
+    public StringBuilder visitLogicalOrExpression(JPPExpressionParser.LogicalOrExpressionContext ctx) {
         return null;
     }
 
+    // logicalAndExpression
+    //    :   primaryExpression (AND primaryExpression)*
+    //    ;
     @Override
-    public StringBuilder visitPrimaryExpression(CExpressionParser.PrimaryExpressionContext ctx) {
+    public StringBuilder visitLogicalAndExpression(JPPExpressionParser.LogicalAndExpressionContext ctx) {
         return null;
     }
 
+    // primaryExpression
+    //    :   definedExpression
+    //    |   undefinedExpression
+    //    |   comparisonExpression
+    //    ;
     @Override
-    public StringBuilder visitSpecialOperator(CExpressionParser.SpecialOperatorContext ctx) {
+    public StringBuilder visitPrimaryExpression(JPPExpressionParser.PrimaryExpressionContext ctx) {
         return null;
     }
 
+    // comparisonExpression
+    //    :   operand ((LT|GT|LEQ|GEQ|EQ|NEQ) operand)?
+    //    ;
     @Override
-    public StringBuilder visitSpecialOperatorArgument(CExpressionParser.SpecialOperatorArgumentContext ctx) {
+    public StringBuilder visitComparisonExpression(JPPExpressionParser.ComparisonExpressionContext ctx) {
         return null;
     }
 
+    // operand
+    //    :   propertyExpression
+    //    |   Constant
+    //    |   StringLiteral+
+    //    |   unaryOperator Constant
+    //    ;
     @Override
-    public StringBuilder visitUnaryOperator(CExpressionParser.UnaryOperatorContext ctx) {
+    public StringBuilder visitOperand(JPPExpressionParser.OperandContext ctx) {
         return null;
     }
 
+    // definedExpression
+    //    :   'defined' '(' Identifier ')'
+    //    ;
     @Override
-    public StringBuilder visitNamespaceExpression(CExpressionParser.NamespaceExpressionContext ctx) {
+    public StringBuilder visitDefinedExpression(JPPExpressionParser.DefinedExpressionContext ctx) {
         return null;
     }
 
+    // undefinedExpression
+    //    :   NOT 'defined' '(' Identifier ')'
+    //    ;
     @Override
-    public StringBuilder visitMultiplicativeExpression(CExpressionParser.MultiplicativeExpressionContext ctx) {
+    public StringBuilder visitUndefinedExpression(JPPExpressionParser.UndefinedExpressionContext ctx) {
         return null;
     }
 
+    // propertyExpression
+    //    :   '${' Identifier '}'
+    //    ;
     @Override
-    public StringBuilder visitAdditiveExpression(CExpressionParser.AdditiveExpressionContext ctx) {
+    public StringBuilder visitPropertyExpression(JPPExpressionParser.PropertyExpressionContext ctx) {
         return null;
     }
 
+    // unaryOperator
+    //    : U_PLUS
+    //    | U_MINUS
+    //    ;
     @Override
-    public StringBuilder visitShiftExpression(CExpressionParser.ShiftExpressionContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitRelationalExpression(CExpressionParser.RelationalExpressionContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitEqualityExpression(CExpressionParser.EqualityExpressionContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitAndExpression(CExpressionParser.AndExpressionContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitExclusiveOrExpression(CExpressionParser.ExclusiveOrExpressionContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitInclusiveOrExpression(CExpressionParser.InclusiveOrExpressionContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitLogicalAndExpression(CExpressionParser.LogicalAndExpressionContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitLogicalOrExpression(CExpressionParser.LogicalOrExpressionContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitLogicalOperand(CExpressionParser.LogicalOperandContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitMacroExpression(CExpressionParser.MacroExpressionContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitArgumentExpressionList(CExpressionParser.ArgumentExpressionListContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitAssignmentExpression(CExpressionParser.AssignmentExpressionContext ctx) {
-        return null;
-    }
-
-    @Override
-    public StringBuilder visitAssignmentOperator(CExpressionParser.AssignmentOperatorContext ctx) {
+    public StringBuilder visitUnaryOperator(JPPExpressionParser.UnaryOperatorContext ctx) {
         return null;
     }
 }
