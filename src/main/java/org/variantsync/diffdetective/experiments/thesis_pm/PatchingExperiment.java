33efa25faae15734f5075bfe174e2f0a1afb55b0
piameier <pia.meier@tu-bs.de>
2025-08-01 10:04:22 +0200

    removed unused imports

--- src/main/java/org/variantsync/diffdetective/experiments/thesis_pm/PatchingExperiment.java
+++ src/main/java/org/variantsync/diffdetective/experiments/thesis_pm/PatchingExperiment.java
@@ -1,32 +1,10 @@
 package org.variantsync.diffdetective.experiments.thesis_pm;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import org.eclipse.jgit.diff.DiffAlgorithm;
-import org.eclipse.jgit.diff.DiffAlgorithm.SupportedAlgorithm;
-import org.variantsync.diffdetective.diff.result.DiffParseException;
 import org.variantsync.diffdetective.show.Show;
 import org.variantsync.diffdetective.show.engine.GameEngine;
-import org.variantsync.diffdetective.util.fide.FixTrueFalse.Formula;
 import org.variantsync.diffdetective.variation.DiffLinesLabel;
-import org.variantsync.diffdetective.variation.diff.DiffNode;
-import org.variantsync.diffdetective.variation.diff.DiffType;
-import org.variantsync.diffdetective.variation.diff.Time;
-import org.variantsync.diffdetective.variation.diff.VariationDiff;
-import org.variantsync.diffdetective.variation.diff.parse.VariationDiffParseOptions;
 import org.variantsync.diffdetective.variation.diff.patching.Patching;
-import org.variantsync.diffdetective.variation.diff.source.VariationDiffSource;
-import org.variantsync.diffdetective.variation.diff.view.DiffView;
 import org.variantsync.diffdetective.variation.tree.VariationTree;
-import org.variantsync.diffdetective.variation.tree.view.relevance.Configure;
-import org.variantsync.diffdetective.variation.tree.view.relevance.Relevance;
 
 public class PatchingExperiment {
 
