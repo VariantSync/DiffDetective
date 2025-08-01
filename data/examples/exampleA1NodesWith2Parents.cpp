170206d5ef404a44ab4e5cf73ca765ae170f8619
piameier <pia.meier@tu-bs.de>
2025-08-01 09:57:23 +0200

    example nodes-with-2-parents

---
+++ data/examples/exampleA1NodesWith2Parents.cpp
@@ -0,0 +1,7 @@
+#ifdef Feature1
+    int x = 7;
+    x += 2;
+#ifdef Feature2
+    x += 3;
+#endif
+#endif
---
+++ data/examples/exampleA2NodesWith2Parents.cpp
@@ -0,0 +1,7 @@
+#ifdef Feature1
+    int x = 7;
+#ifdef Feature2
+    x += 2;
+    x += 3;
+#endif
+#endif
