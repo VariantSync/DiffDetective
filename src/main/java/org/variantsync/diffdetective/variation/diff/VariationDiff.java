c9f05da9e1250e04f693c1ead3eb814e6bfb1bdf
pmbittner <paul.bittner@uni-ulm.de>
2025-09-23 14:32:24 +0200

    feat: VariationDiff::computeAllFeatureNames()

--- src/main/java/org/variantsync/diffdetective/variation/diff/VariationDiff.java
+++ src/main/java/org/variantsync/diffdetective/variation/diff/VariationDiff.java
@@ -36,8 +36,10 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
@@ -374,6 +376,24 @@ public class VariationDiff<L extends Label> {
             }
         });
         return count.get();
+    }
+
+
+    /**
+     * Returns all variable names occurring in annotations (i.e., formulas of mapping nodes) in this variation diff.
+     * @return A set of every occuring feature name.
+     */
+    public Set<String> computeAllFeatureNames() {
+        Set<String> features = new HashSet<>();
+        forAll(node -> {
+                if (node.isConditionalAnnotation()) {
+                    features.addAll(node.getFormula().getUniqueContainedFeatures());
+                }
+            });
+        // Since FeatureIDE falsely reports constants "True" and "False" as feature names, we have to remove them from the resulting set.
+        features.removeIf(FixTrueFalse::isTrueLiteral);
+        features.removeIf(FixTrueFalse::isFalseLiteral);
+        return features;
     }
 
     /**
