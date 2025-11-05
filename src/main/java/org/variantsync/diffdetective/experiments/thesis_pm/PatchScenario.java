ec7816d02c699b185b43e748719472acb20ba551
piameier <pia.meier@tu-bs.de>
2025-11-05 22:15:43 +0100

    use view on source patch configured with target for unchanged

--- src/main/java/org/variantsync/diffdetective/experiments/thesis_pm/PatchScenario.java
+++ src/main/java/org/variantsync/diffdetective/experiments/thesis_pm/PatchScenario.java
@@ -1,9 +1,12 @@
 package org.variantsync.diffdetective.experiments.thesis_pm;
 
+import org.variantsync.diffdetective.show.Show;
+import org.variantsync.diffdetective.show.engine.GameEngine;
 import org.variantsync.diffdetective.variation.DiffLinesLabel;
 import org.variantsync.diffdetective.variation.Label;
 import org.variantsync.diffdetective.variation.diff.Time;
 import org.variantsync.diffdetective.variation.diff.VariationDiff;
+import org.variantsync.diffdetective.variation.diff.view.DiffView;
 import org.variantsync.diffdetective.variation.tree.VariationTree;
 import org.variantsync.diffdetective.variation.tree.view.TreeView;
 import org.variantsync.diffdetective.variation.tree.view.relevance.Configure;
@@ -30,8 +33,10 @@ public class PatchScenario<L extends Label> {
 		this.sourceVariantConfig = sourceVariantConfig;
 		this.targetVariantConfig = targetVariantConfig;
 		this.sourceVariantAfterRedToCrossVarFeatures = (VariationTree<DiffLinesLabel>) TreeView.tree(sourcePatch.project(Time.AFTER), this.targetVariantConfig);
-		this.unchangedAfter = new Unchanged((VariationDiff<DiffLinesLabel>) sourcePatch, Time.AFTER);
-		Unchanged unchangedBefore = new Unchanged((VariationDiff<DiffLinesLabel>) sourcePatch, Time.BEFORE);
+		VariationDiff<DiffLinesLabel> sourcePatchConfiguredToCrossVarFeatures = (VariationDiff<DiffLinesLabel>) DiffView.optimized(sourcePatch, targetVariantConfig);
+		this.unchangedAfter = new Unchanged((VariationDiff<DiffLinesLabel>) sourcePatchConfiguredToCrossVarFeatures, Time.AFTER);		
+		Unchanged unchangedBefore = new Unchanged((VariationDiff<DiffLinesLabel>) sourcePatchConfiguredToCrossVarFeatures, Time.BEFORE);
 		this.targetVariantBeforeRedToUnchanged = (VariationTree<DiffLinesLabel>) TreeView.tree(this.targetVariantBefore, unchangedBefore);
+		GameEngine.showAndAwaitAll(Show.diff(sourcePatch), Show.diff(sourcePatchConfiguredToCrossVarFeatures), Show.tree(targetVariantBeforeRedToUnchanged));
 	}
 }
