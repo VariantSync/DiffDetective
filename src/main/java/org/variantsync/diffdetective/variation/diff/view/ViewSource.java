a3511e1e67458c2bc99b83fac8265aa4e30c0336
pmbittner <paul.bittner@uni-ulm.de>
2025-11-06 10:35:57 +0100

    make ViewSource applicable to trees as well

--- src/main/java/org/variantsync/diffdetective/variation/diff/view/ViewSource.java
+++ src/main/java/org/variantsync/diffdetective/variation/diff/view/ViewSource.java
@@ -3,17 +3,15 @@ package org.variantsync.diffdetective.variation.diff.view;
 import java.util.List;
 
 import org.variantsync.diffdetective.util.Source;
-import org.variantsync.diffdetective.variation.Label;
-import org.variantsync.diffdetective.variation.diff.VariationDiff;
 import org.variantsync.diffdetective.variation.tree.view.relevance.Relevance;
 
 /**
- * A {@link Source} that remembers that a variation diff represents a view on another variation
- * diff.
- * @param diff The original variation diff on which the variation diff with this source is a view on.
+ * A {@link Source} that remembers that a variation tree or diff represents a view on another variation
+ * tree or diff.
+ * @param target The original variation tree/diff on which the view was created.
  * @param relevance The relevance predicate that was used to create the view.
  */
-public record ViewSource<L extends Label>(VariationDiff<L> diff, Relevance relevance, String method) implements Source {
+public record ViewSource(Source target, Relevance relevance, String method) implements Source {
     @Override
     public String getSourceExplanation() {
         return "view";
@@ -21,7 +19,7 @@ public record ViewSource<L extends Label>(VariationDiff<L> diff, Relevance relev
 
     @Override
     public List<Source> getSources() {
-        return List.of(diff);
+        return List.of(target);
     }
 
     @Override
